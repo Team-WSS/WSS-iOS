8693bf5ec820a06a0586649224fac922b3b76eed
shinzz410 <tlswldnjs410@gmail.com>
2024-01-01 21:27:31 +0900

    [Setting] #1 - 프로젝트 생성

---
+++ WSSiOS/WSSiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 1/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
