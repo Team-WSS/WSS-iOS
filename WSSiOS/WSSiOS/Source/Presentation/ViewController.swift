6ea4c80db236fc6227dc92c6887ffbd32551fcff
shinzz410 <tlswldnjs410@gmail.com>
2024-01-03 23:24:04 +0900

    [Test] #1 - Font, Color Test 코드 추가

--- WSSiOS/WSSiOS/Source/Presentation/ViewController.swift
+++ WSSiOS/WSSiOS/Source/Presentation/ViewController.swift
@@ -7,11 +7,27 @@
 
 import UIKit
 
+import SnapKit
+
 class ViewController: UIViewController {
 
+    var testLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test 입니다."
+        label.font = .Label1
+        label.textColor = .Gray300
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .GrayToast
+        self.view.addSubview(testLabel)
+        
+        testLabel.snp.makeConstraints() {
+            $0.centerX.centerY.equalToSuperview()
+        }
     }
 
 
