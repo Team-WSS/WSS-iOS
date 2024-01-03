f008cdf68bc83ca531efb3a90f98d380ade2d28d
shinzz410 <tlswldnjs410@gmail.com>
2024-01-03 23:26:30 +0900

    [Del] #1 - Font, Color Test 코드 삭제

--- WSSiOS/WSSiOS/Source/Presentation/ViewController.swift
+++ WSSiOS/WSSiOS/Source/Presentation/ViewController.swift
@@ -11,23 +11,10 @@ import SnapKit
 
 class ViewController: UIViewController {
 
-    var testLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Test 입니다."
-        label.font = .Label1
-        label.textColor = .Gray300
-        return label
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .GrayToast
-        self.view.addSubview(testLabel)
-        
-        testLabel.snp.makeConstraints() {
-            $0.centerX.centerY.equalToSuperview()
-        }
     }
 
 
