e54312242a51bcca08d28c8597731f01b0eefcac
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2024-01-06 02:34:04 +0900

    [Add] #11 - SearchWebNovelVC에 필요한 뷰 생성

---
+++ WSSiOS/WSSiOS/Source/Presentation/Search/SearchWebNovelViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchWebNovelViewController.swift
+//  WSSiOS
+//
+//  Created by 최서연 on 1/6/24.
+//
+
+import UIKit
+
+final class SearchWebNovelViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ WSSiOS/WSSiOS/Source/Presentation/Search/View/SearchResultView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultView.swift
+//  WSSiOS
+//
+//  Created by 최서연 on 1/6/24.
+//
+
+import UIKit
+
+final class SearchResultView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
