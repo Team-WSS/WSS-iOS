06d6a668a2b2e9362d92ff8ddb77845d711d5b8c
shinzz410 <tlswldnjs410@gmail.com>
2024-01-07 00:02:13 +0900

    [Add] #12 - TabBarItem CaseIterable 추가

---
+++ WSSiOS/WSSiOS/Source/Presentation/Base/WSSTabBarItem.swift
@@ -0,0 +1,17 @@
+//
+//  WSSTabBarItem.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 1/7/24.
+//
+
+import UIKit
+
+enum WSSTabBarItem: CaseIterable {
+    
+    case home, library, record, myPage
+    
+    //로고, 타이틀, 뷰컨, 선택유무
+    
+    
+}
