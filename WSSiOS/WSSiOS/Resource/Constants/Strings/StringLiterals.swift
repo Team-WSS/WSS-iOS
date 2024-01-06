93edc12605489712422a2637cf64244e239febc4
shinzz410 <tlswldnjs410@gmail.com>
2024-01-07 00:28:23 +0900

    [Add] #12 - StringLiterals 추가

---
+++ WSSiOS/WSSiOS/Resource/Constants/Strings/StringLiterals.swift
@@ -0,0 +1,20 @@
+//
+//  StringLiterals.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 1/7/24.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+    enum Tabbar {
+        enum Title {
+            static let home = "홈"
+            static let libarary = "서재"
+            static let record = "기록"
+            static let myPage = "MY"
+        }
+    }
+}
