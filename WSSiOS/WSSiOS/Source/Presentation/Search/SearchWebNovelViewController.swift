aa2f2ce4c7a13205c9dbe32a335c9c6362906e52
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2024-01-07 02:13:51 +0900

    [Design] #11 - SearchBar 내 TextField UI 구현

--- WSSiOS/WSSiOS/Source/Presentation/Search/SearchWebNovelViewController.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Search/SearchWebNovelViewController.swift
@@ -7,9 +7,42 @@
 
 import UIKit
 
-final class SearchWebNovelViewController: UIViewController {
+final class SearchWebNovelViewController: UIViewController, UISearchBarDelegate {
+    
+    //MARK: set Properties
+    
+    private let headerView = SearchHeaderView()
+    
+    //MARK: Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+        setHierachy()
+        setLayout()
+    }
+    
+    //MARK: set UI
+    
+    private func setUI() {
+        self.view.backgroundColor = .White
+        headerView.searchBar.delegate = self
+    }
+    
+    //MARK: set Hierachy
+    
+    private func setHierachy() {
+        self.view.addSubview(headerView)
+    }
+    
+    //MARK: set Layout
+    
+    private func setLayout() {
+        headerView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(50)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(67)
+        }
     }
 }
--- WSSiOS/WSSiOS/Source/Presentation/Search/View/SearchHeaderView.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Search/View/SearchHeaderView.swift
@@ -7,14 +7,67 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 final class SearchHeaderView: UIView {
+    
+    //MARK: set Properties
+    
+    let searchBar = UISearchBar()
+    
+    //MARK: Life Cycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        setUI()
+        setHierachy()
+        setLayout()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: set UI
+    
+    private func setUI() {
+        self.do {
+            $0.backgroundColor = .white
+        }
+        
+        searchBar.do {
+            $0.setImage(ImageLiterals.icon.search, for: .search, state: .normal)
+            $0.layer.borderColor = UIColor.Gray200.cgColor
+            $0.layer.borderWidth = 1
+            $0.clipsToBounds = true
+            $0.layer.cornerRadius = 12
+        }
+        
+        if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
+            textfield.backgroundColor = .White
+            textfield.font = .Body2
+            textfield.attributedPlaceholder = NSAttributedString(string: textfield.placeholder ?? "등록할 작품 검색하기", attributes: [NSAttributedString.Key.foregroundColor : UIColor.Gray200])
+            // 텍스트 입력시 색상
+            textfield.textColor = UIColor.Black
+        }
+    }
+    
+    //MARK: set Hierachy
+    
+    private func setHierachy() {
+        self.addSubview(searchBar)
+    }
+    
+    //MARK: set Layout
+    
+    private func setLayout() {
+        searchBar.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(44)
+        }
     }
 }
