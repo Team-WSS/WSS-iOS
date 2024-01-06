784e92ff291f300473b8666e8a931c98542bec61
shinzz410 <tlswldnjs410@gmail.com>
2024-01-06 22:58:29 +0900

    [Add] #12 - TabBar 파일 생성

---
+++ WSSiOS/WSSiOS/Source/Presentation/Base/WSSTabBarController.swift
@@ -0,0 +1,40 @@
+//
+//  WSSTabBarController.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 1/6/24.
+//
+
+import UIKit
+
+class WSSTabBarController: UITabBarController {
+
+    //MARK: Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setUI()
+        setHierachy()
+        setLayout()
+    }
+    
+    //MARK: UI
+    
+    private func setUI() {
+        
+    }
+    
+    //MARK: Hierachy
+    
+    private func setHierachy() {
+        
+    }
+    
+    //MARK: Layout
+    
+    private func setLayout() {
+        
+    }
+
+}
