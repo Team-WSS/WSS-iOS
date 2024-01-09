8d90c95c96ee9a273a1c473f67b28560214b3278
shinzz410 <tlswldnjs410@gmail.com>
2024-01-09 17:20:58 +0900

    [Design] #21 - MainButton Layout 구현

--- WSSiOS/WSSiOS/Source/Presentation/Base/WSSMainButton.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Base/WSSMainButton.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
+import SnapKit
+
 class WSSMainButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     
-    init(title: String) {
+    init(title: String, selfY: Bool) {
         super.init(frame: .zero)
         
         setTitle(title, for: .normal)
@@ -21,11 +23,20 @@ class WSSMainButton: UIButton {
         setTitleColor(.White, for: .normal)
         backgroundColor = .Primary100
         layer.cornerRadius = 12
+        
+        self.snp.makeConstraints() {
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().inset(20)
+            $0.height.equalTo(53)
+            
+            if !selfY {
+                $0.bottom.equalTo(super.safeAreaInsets.bottom)
+            }
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
     
 }
