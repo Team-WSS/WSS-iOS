733d9367f41270f4bd675942c0b8ec4e7f6d8aeb
YunHak Lee <lyh990408@gmail.com>
2024-01-10 15:23:06 +0900

    [Add] #15 - RegisterNormalView에 필요한 초기 파일&폴더링 추가, UIImageViewAlignedSwift 라이브러리 추가

---
+++ WSSiOS/WSSiOS/Resource/Constants/Strings/RegisterNormalView.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterNormalView.swift
+//  WSSiOS
+//
+//  Created by 이윤학 on 1/10/24.
+//
+
+import Foundation
---
+++ WSSiOS/WSSiOS/Source/Presentation/RegisterNormal/RegisterNormalViewController/RegisterNormalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterNormalViewController.swift
+//  WSSiOS
+//
+//  Created by 이윤학 on 1/10/24.
+//
+
+import UIKit
+
+class RegisterNormalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
