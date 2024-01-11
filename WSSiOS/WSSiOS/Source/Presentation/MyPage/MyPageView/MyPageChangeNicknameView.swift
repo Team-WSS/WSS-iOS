4301c3e95e5fce236c29bad4c1ba153ad8e06adb
shinzz410 <tlswldnjs410@gmail.com>
2024-01-12 01:46:09 +0900

    [Add] #27 - View, VC 생성

---
+++ WSSiOS/WSSiOS/Source/Presentation/MyPage/MyPageView/MyPageChangeNicknameView.swift
@@ -0,0 +1,41 @@
+//
+//  MyPageChangeNicknameView.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 1/12/24.
+//
+
+import UIKit
+
+class MyPageChangeNicknameView: UIView {
+
+    //MARK: - UI Components
+    
+   
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierachy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+       
+    }
+    
+    private func setHierachy() {
+    
+    }
+    
+    private func setLayout() {
+        
+    }
+}
---
+++ WSSiOS/WSSiOS/Source/Presentation/MyPage/MyPageViewController/MyPageChangeNicknameViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MyPageChangeNicknameViewController.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 1/12/24.
+//
+
+import UIKit
+
+class MyPageChangeNicknameViewController: UIViewController {
+
+    //MARK: - Set Properties
+
+    
+    //MARK: - UI Components
+    
+    private var rootView = MyPageChangeNicknameView()
+    
+    // MARK: - Life Cycle
+    
+    override func loadView() {
+        self.view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    //MARK: - UI Components
+    
+
+    
+    //MARK: - Custom Method
+
+}
