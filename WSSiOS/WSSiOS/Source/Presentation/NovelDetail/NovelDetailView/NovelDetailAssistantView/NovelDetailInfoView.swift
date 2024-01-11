b3696f10fedd4f729c972370f7cbc6bca7dfbea9
Hyowon Jeon <hyowon612@gmail.com>
2024-01-12 00:40:42 +0900

    [Design] #30 - NovelDetailInfoRatingView UI 구현

---
+++ WSSiOS/WSSiOS/Source/Presentation/NovelDetail/NovelDetailView/NovelDetailAssistantView/NovelDetailInfoView.swift
@@ -0,0 +1,57 @@
+//
+//  NovelDetailInfoView.swift
+//  WSSiOS
+//
+//  Created by Hyowon Jeon on 1/11/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class NovelDetailInfoView: UIView {
+
+    // MARK: - UI Components
+    
+    let contentView = UIStackView()
+    let novelDetailInfoRatingView = NovelDetailInfoRatingView()
+
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierachy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - set UI
+    
+    private func setUI() {
+        contentView.do {
+            $0.alignment = .fill
+            $0.axis = .vertical
+        }
+    }
+    
+    // MARK: - set Hierachy
+    
+    private func setHierachy() {
+        self.addSubview(contentView)
+        contentView.addArrangedSubviews(novelDetailInfoRatingView)
+    }
+    
+    // MARK: - set Layout
+    
+    private func setLayout() {
+        contentView.snp.makeConstraints {
+            $0.edges.width.equalToSuperview()
+        }
+    }
+}
