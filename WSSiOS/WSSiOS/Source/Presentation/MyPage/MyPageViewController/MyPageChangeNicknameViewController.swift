a8831aff8d85c0c369bff0c2d73ddc2eddae2266
shinzz410 <tlswldnjs410@gmail.com>
2024-01-12 03:15:30 +0900

    [Feat] #27 - TextField 기능 구현

--- WSSiOS/WSSiOS/Source/Presentation/MyPage/MyPageViewController/MyPageChangeNicknameViewController.swift
+++ WSSiOS/WSSiOS/Source/Presentation/MyPage/MyPageViewController/MyPageChangeNicknameViewController.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 class MyPageChangeNicknameViewController: UIViewController {
-
+    
     //MARK: - Set Properties
-
+    
+    private let disposeBag = DisposeBag()
     
     //MARK: - UI Components
     
@@ -24,13 +28,46 @@ class MyPageChangeNicknameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textFieldEvent()
+    }
 
+    //MARK: - Custom Method
+    
+    private func textFieldEvent() {
+        rootView.changeNicknameTextField.rx.controlEvent([.editingDidBegin, .editingChanged])
+            .asObservable()
+            .subscribe(onNext: { _ in
+                self.rootView.textFieldUnderBarView.backgroundColor = .Primary100
+            })
+            .disposed(by: disposeBag)
+        
+        rootView.changeNicknameTextField.rx.controlEvent([.editingDidEnd])
+            .asObservable()
+            .subscribe(onNext: { _ in
+                self.rootView.textFieldUnderBarView.backgroundColor = .Gray200
+            })
+            .disposed(by: disposeBag)
+        
+        rootView.changeNicknameTextField.rx.text
+            .subscribe(onNext: { text in
+                if let countText = text?.count {
+                    self.rootView.countNicknameLabel.text = "\(countText)/10"
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        rootView.changeNicknameTextField.rx.text.orEmpty
+            .subscribe(onNext: { text in 
+                self.limitNum(text)
+            })
+            .disposed(by: disposeBag)
     }
     
-    //MARK: - UI Components
-    
-
-    
-    //MARK: - Custom Method
-
+    private func limitNum(_ text: String) {
+        if text.count > 9 {
+            let index = text.index(text.startIndex, offsetBy: 9)
+            self.rootView.changeNicknameTextField.text = String(text[..<index])
+        }
+    }
 }
