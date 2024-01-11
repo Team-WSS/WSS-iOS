35d2b71777789655de0672df579f89371a29946b
Hyowon Jeon <hyowon612@gmail.com>
2024-01-11 17:42:52 +0900

    [Add] #30 - NovelDetailVC에 필요한 파일 생성

---
+++ WSSiOS/WSSiOS/Source/Presentation/NovelDetail/NovelDetailView/NovelDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  NovelDetailView.swift
+//  WSSiOS
+//
+//  Created by Hyowon Jeon on 1/11/24.
+//
+
+import UIKit
+
+class NovelDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ WSSiOS/WSSiOS/Source/Presentation/NovelDetail/NovelDetailViewController/NovelDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NovelDetailViewController.swift
+//  WSSiOS
+//
+//  Created by Hyowon Jeon on 1/11/24.
+//
+
+import UIKit
+
+class NovelDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
