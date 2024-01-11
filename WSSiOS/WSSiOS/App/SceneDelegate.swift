5365f8067504bbf2bca04162c6635a19699cde29
shinzz410 <tlswldnjs410@gmail.com>
2024-01-12 02:18:13 +0900

    [Design] #27 - 닉네임 변경 UI 구현

--- WSSiOS/WSSiOS/App/SceneDelegate.swift
+++ WSSiOS/WSSiOS/App/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let navigationController = UINavigationController(rootViewController: WSSTabBarController())
+        let navigationController = UINavigationController(rootViewController: MyPageChangeNicknameViewController())
         
         self.window = UIWindow(windowScene: windowScene)
         self.window?.rootViewController = navigationController
--- WSSiOS/WSSiOS/Source/Presentation/MyPage/MyPageView/MyPageChangeNicknameView.swift
+++ WSSiOS/WSSiOS/Source/Presentation/MyPage/MyPageView/MyPageChangeNicknameView.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
-class MyPageChangeNicknameView: UIView {
+import SnapKit
+import Then
 
+class MyPageChangeNicknameView: UIView {
+    
     //MARK: - UI Components
     
-   
+    private let nicknameLabel = UILabel()
+    var changeNicknameTextField = UITextField()
+    var textFieldUnderBarView = UIView()
+    var countNicknameLabel = UILabel()
     
     // MARK: - Life Cycle
     
@@ -28,14 +34,60 @@ class MyPageChangeNicknameView: UIView {
     }
     
     private func setUI() {
-       
+        self.backgroundColor = .White
+        
+        nicknameLabel.do {
+            $0.text = "닉네임"
+            $0.font = .Body2
+            $0.textColor = .Gray200
+        }
+        
+        changeNicknameTextField.do {
+            $0.text = "김명진"
+            $0.font = .Body1
+            $0.textColor = .Black
+            $0.borderStyle = .none
+        }
+        
+        textFieldUnderBarView.do {
+            $0.backgroundColor = .Gray200
+        }
+        
+        countNicknameLabel.do {
+            $0.text = "3/10"
+            $0.font = .Label1
+            $0.textColor = .Gray200
+        }
     }
     
     private func setHierachy() {
-    
+        self.addSubviews(nicknameLabel,
+                         changeNicknameTextField,
+                         textFieldUnderBarView,
+                         countNicknameLabel)
     }
     
     private func setLayout() {
+        nicknameLabel.snp.makeConstraints() {
+            $0.top.leading.equalTo(safeAreaLayoutGuide).offset(20)
+        }
         
+        changeNicknameTextField.snp.makeConstraints() {
+            $0.top.equalTo(nicknameLabel.snp.bottom)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(41)
+        }
+        
+        textFieldUnderBarView.snp.makeConstraints() {
+            $0.bottom.equalTo(changeNicknameTextField.snp.bottom)
+            $0.leading.equalTo(changeNicknameTextField.snp.leading)
+            $0.width.equalTo(changeNicknameTextField.snp.width)
+            $0.height.equalTo(1.3)
+        }
+        
+        countNicknameLabel.snp.makeConstraints() {
+            $0.top.equalTo(changeNicknameTextField.snp.bottom).offset(10)
+            $0.trailing.equalTo(changeNicknameTextField.snp.trailing)
+        }
     }
 }
