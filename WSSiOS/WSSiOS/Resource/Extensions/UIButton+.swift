bec18d17c3bcba885cadea42e2059400ccc1a80e
Hyowon Jeon <hyowon612@gmail.com>
2024-01-13 12:43:53 +0900

    [Add] #30 - DifferentRadiusButton 추가

--- WSSiOS/WSSiOS/Resource/Extensions/UIButton+.swift
+++ WSSiOS/WSSiOS/Resource/Extensions/UIButton+.swift
@@ -14,3 +14,49 @@ extension UIButton {
         self.setAttributedTitle(attributedString, for: .normal)
     }
 }
+
+final class DifferentRadiusButton: UIButton {
+    var topLeftRadius: CGFloat = 0.0
+    var topRightRadius: CGFloat = 0.0
+    var bottomLeftRadius: CGFloat = 0.0
+    var bottomRightRadius: CGFloat = 0.0
+
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath()
+
+        path.move(to: CGPoint(x: rect.minX + topLeftRadius, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - topRightRadius, y: rect.minY))
+        path.addArc(withCenter: CGPoint(x: rect.maxX - topRightRadius, y: rect.minY + topRightRadius),
+                    radius: topRightRadius,
+                    startAngle: CGFloat(-Double.pi / 2),
+                    endAngle: 0,
+                    clockwise: true)
+
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - bottomRightRadius))
+        path.addArc(withCenter: CGPoint(x: rect.maxX - bottomRightRadius, y: rect.maxY - bottomRightRadius),
+                    radius: bottomRightRadius,
+                    startAngle: 0,
+                    endAngle: CGFloat(Double.pi / 2),
+                    clockwise: true)
+
+        path.addLine(to: CGPoint(x: rect.minX + bottomLeftRadius, y: rect.maxY))
+        path.addArc(withCenter: CGPoint(x: rect.minX + bottomLeftRadius, y: rect.maxY - bottomLeftRadius),
+                    radius: bottomLeftRadius,
+                    startAngle: CGFloat(Double.pi / 2),
+                    endAngle: CGFloat(Double.pi),
+                    clockwise: true)
+
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + topLeftRadius))
+        path.addArc(withCenter: CGPoint(x: rect.minX + topLeftRadius, y: rect.minY + topLeftRadius),
+                    radius: topLeftRadius,
+                    startAngle: CGFloat(Double.pi),
+                    endAngle: CGFloat(-Double.pi / 2),
+                    clockwise: true)
+
+        path.close()
+
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = path.cgPath
+        layer.mask = maskLayer
+    }
+}
