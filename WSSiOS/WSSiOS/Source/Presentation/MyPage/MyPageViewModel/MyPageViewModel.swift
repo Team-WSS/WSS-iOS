d3e91393e2f2a5c490ad138a1691e5733bcd20da
shinzz410 <tlswldnjs410@gmail.com>
2024-01-15 17:34:45 +0900

    [Add] #55 - MyPageViewModel 생성

--- WSSiOS/WSSiOS/Source/Presentation/MyPage/MyPageViewModel/MyPageViewModel.swift
+++ WSSiOS/WSSiOS/Source/Presentation/MyPage/MyPageViewModel/MyPageViewModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+public struct MyPageViewModel: Decodable {
+    public let userNickName: String
+    public let userNovelCount, memoCount: Int
+    
+    public init(userNickName: String, userNovelCount: Int, memoCount: Int) {
+        self.userNickName = userNickName
+        self.userNovelCount = userNovelCount
+        self.memoCount = memoCount
+    }
+}
