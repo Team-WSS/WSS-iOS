d66aed8172877e2ac3214f688c2d9352203ba02d
shinzz410 <tlswldnjs410@gmail.com>
2024-01-17 02:34:58 +0900

    [Add] #55 - Repository, Service 추가

--- WSSiOS/WSSiOS/Network/Avatar/AvatarService.swift
+++ WSSiOS/WSSiOS/Network/Avatar/AvatarService.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+import RxSwift
+
+protocol AvatarService {
+    func getAvatarData() -> Single<AvatarResult>
+}
+
+final class DefaultAvatarService: NSObject, Networking {
+    private let avatarListQueryItems: [URLQueryItem] = [URLQueryItem(name: "avatarId", value: String(describing: 10))]
+    private var urlSession: URLSession = URLSession(configuration: URLSessionConfiguration.default,
+                                                    delegate: nil,
+                                                    delegateQueue: nil)
+}
+
+extension DefaultAvatarService: AvatarService {
+    func getAvatarData() -> RxSwift.Single<AvatarResult> {
+        let request = try! makeHTTPRequest(method: .get,
+                                           path: URLs.Avatar.getAvatarDetail,
+                                           queryItems: avatarListQueryItems,
+                                           headers: APIConstants.testTokenHeader,
+                                           body: nil)
+        
+        NetworkLogger.log(request: request)
+        
+        return urlSession.rx.data(request: request)
+            .map { try self.decode(data: $0,
+                                   to: AvatarResult.self) }
+            .asSingle()
+    }
+}
+
+
--- WSSiOS/WSSiOS/Source/Data/Repository/AvatarRepository.swift
+++ WSSiOS/WSSiOS/Source/Data/Repository/AvatarRepository.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+import RxSwift
+
+protocol AvatarRepository {
+    func getUserData() -> Observable<AvatarResult>
+}
+
+struct DefaultAvatarRepository: AvatarRepository {
+    
+    private var avatarService: AvatarService
+    
+    init(avatarService: AvatarService) {
+        self.avatarService = avatarService
+    }
+    
+    func getUserData() -> RxSwift.Observable<AvatarResult> {
+        return avatarService.getAvatarData()
+            .asObservable()
+    }
+}
+
