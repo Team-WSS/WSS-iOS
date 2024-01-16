6e99196b7899d571545f9b78387a83eac7288dba
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2024-01-16 22:37:45 +0900

    [Add] #64 - UserService 파일 추가

--- WSSiOS/WSSiOS/Network/User/UserService.swift
+++ WSSiOS/WSSiOS/Network/User/UserService.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+import RxSwift
+
+protocol UserService {
+    func getUserCharacter() -> Single<UserCharacter>
+}
+
+final class DefaultUserService: NSObject, Networking {
+    
+    private var urlSession = URLSession(configuration: URLSessionConfiguration.default,
+                                        delegate: nil,
+                                        delegateQueue: nil)
+}
+
+extension DefaultUserService: UserService {
+    func getUserCharacter() -> Single<UserCharacter> {
+        let request = try! makeHTTPRequest(method: .get,
+                                           path: URLs.Memo.getMemoList,
+                                           headers: APIConstants.testTokenHeader,
+                                           body: nil)
+        
+        NetworkLogger.log(request: request)
+        
+        return urlSession.rx.data(request: request)
+            .map { try self.decode(data: $0, to: UserCharacter.self) }
+            .asSingle()
+    }
+}
