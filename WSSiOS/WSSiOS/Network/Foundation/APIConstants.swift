7210c0db4dbe97707cbeb1bf6483ce2e7505dc0d
YunHak Lee <lyh990408@gmail.com>
2024-01-31 00:41:49 +0900

    [Fix] #111 - 기존 토큰 만료로 인해 새로운 토큰으로 임시 변경

--- WSSiOS/WSSiOS/Network/Foundation/APIConstants.swift
+++ WSSiOS/WSSiOS/Network/Foundation/APIConstants.swift
@@ -16,7 +16,7 @@ struct APIConstants {
     static let fcm = "FcmToken"
     
     static let boundary = "Boundary-\(UUID().uuidString)"
-    static let testToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDU0MTUxODQsImV4cCI6MTcwNjI3OTE4NCwidXNlcklkIjoyfQ.BSIfqyxKVjbpFLoMOFM5KWSkkJnowXzDt1x6lIrJqDs"
+    static let testToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDY2MjQ4NTMsImV4cCI6MTcwNzQ4ODg1MywidXNlcklkIjo0fQ.3cWJuSUh9cGNaJ6H39VK_ziWyh5qfJzENtTz3HZfkAA"
 }
 
 extension APIConstants {
