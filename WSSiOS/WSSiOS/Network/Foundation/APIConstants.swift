d6bcd214451f397424081836767db2a301f7e9fc
shinzz410 <tlswldnjs410@gmail.com>
2024-01-31 15:10:32 +0900

    [Fix] #111 - TestToken 코드 추가

--- WSSiOS/WSSiOS/Network/Foundation/APIConstants.swift
+++ WSSiOS/WSSiOS/Network/Foundation/APIConstants.swift
@@ -16,7 +16,7 @@ struct APIConstants {
     static let fcm = "FcmToken"
     
     static let boundary = "Boundary-\(UUID().uuidString)"
-    static let testToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDU0MTUxODQsImV4cCI6MTcwNjI3OTE4NCwidXNlcklkIjoyfQ.BSIfqyxKVjbpFLoMOFM5KWSkkJnowXzDt1x6lIrJqDs"
+    static let testToken = Bundle.main.object(forInfoDictionaryKey: Config.Keys.Plist.testToken) as? String ?? ""
 }
 
 extension APIConstants {
