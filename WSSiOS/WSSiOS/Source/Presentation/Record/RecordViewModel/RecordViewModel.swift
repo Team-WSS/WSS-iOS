2e4188ad10eec4f30e676e0acac3038e9230524e
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2024-02-20 00:17:49 +0900

    [Refactor] #125 - RecordViewModel 기본 틀 생성

--- WSSiOS/WSSiOS/Source/Presentation/Record/RecordViewModel/RecordViewModel.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Record/RecordViewModel/RecordViewModel.swift
@@ -2,7 +2,61 @@
 //  RecordViewModel.swift
 //  WSSiOS
 //
-//  Created by 최서연 on 2/15/24.
+//  Created by Seoyeon Choi on 2/19/24.
 //
 
 import Foundation
+
+import RxSwift
+import RxCocoa
+
+protocol ViewModelType {
+    
+    associatedtype Input
+    associatedtype Output
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output
+}
+
+final class RecordViewModel: ViewModelType {
+    
+    //MARK: - Properties
+    
+    private let memoRepository: DefaultMemoRepository
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - Inputs
+    
+    struct Input {
+        let sortTypeButtonTapped: Driver<Void>
+        let newestButtonTapped: Driver<Void>
+        let oldestButtonTapped: Driver<Void>
+        let cellTapped: Driver<IndexPath>
+    }
+    
+    //MARK: - Outputs
+    
+    struct Output {
+        let recordMomoCount: BehaviorRelay<Int>
+        let recordItem: BehaviorRelay<[RecordTableViewCell]>
+    }
+    
+    //MARK: - init
+    
+    init(memoRepository: DefaultMemoRepository) {
+        self.memoRepository = memoRepository
+    }
+}
+
+//MARK: - Methods
+
+extension RecordViewModel {
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        
+        input.sortTypeButtonTapped
+            .drive(with: self, onNext: { owner, _ in
+                
+            })
+            .disposed(by: disposeBag)
+    }
+}
