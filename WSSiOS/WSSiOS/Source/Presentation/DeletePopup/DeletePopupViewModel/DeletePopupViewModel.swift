89fb45e74861d79d992247a26488cbbe154a7c23
Hyowon Jeon <hyowon612@gmail.com>
2024-02-25 17:28:07 +0900

    [Refactor] #125 - DeletePopupViewModel 생성

---
+++ WSSiOS/WSSiOS/Source/Presentation/DeletePopup/DeletePopupViewModel/DeletePopupViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  DeletePopupViewModel.swift
+//  WSSiOS
+//
+//  Created by Hyowon Jeon on 2/25/24.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+final class DeletePopupViewModel: ViewModelType {
+
+    struct Input {
+    }
+
+    struct Output {
+    }
+
+    func transform(from input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+
+        return output
+    }
+}
