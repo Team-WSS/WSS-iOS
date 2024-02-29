52404a76be170e0d93b449b783ab94ad93a3b041
SeoYeon Choi <102604192+choiseoo@users.noreply.github.com>
2024-02-29 17:18:36 +0900

    [Chore] #125 - SearchViewModel 폴더 및 파일 생성

---
+++ WSSiOS/WSSiOS/Source/Presentation/Search/SearchViewModel/SearchViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  SearchViewModel.swift
+//  WSSiOS
+//
+//  Created by Seoyeon Choi on 2/29/24.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+final class SearchViewModel: ViewModelType {
+
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(from input: Input, disposeBag: RxSwift.DisposeBag) -> Output {
+        
+    }
+    
+}
