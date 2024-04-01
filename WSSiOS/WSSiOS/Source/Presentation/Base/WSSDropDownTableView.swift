9fdef9c42d7b639ae681ac6410dab84efa34c3b8
shinzz410 <tlswldnjs410@gmail.com>
2024-04-01 16:00:40 +0900

    [Design] #139 - DropDownTableView 구현

---
+++ WSSiOS/WSSiOS/Source/Presentation/Base/WSSDropDownTableView.swift
@@ -0,0 +1,84 @@
+//
+//  WSSDropDownTableView.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 4/1/24.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+
+final class PuzzleDropdownTableView: UIView {
+    
+    // MARK: - Properties
+    
+    var dropdownData: BehaviorSubject<[String]> = BehaviorSubject(value: [])
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    
+    let dropdownTableView = UITableView()
+    
+    // MARK: - Life Cycles
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .clear
+        
+        setDropDown()
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI
+    
+    private func setUI() {
+        dropdownTableView.do {
+            $0.backgroundColor = .wssWhite
+            $0.layer.cornerRadius = 12
+            $0.layer.masksToBounds = true
+            $0.register(WSSDropdownTableViewCell.self, forCellReuseIdentifier: WSSDropdownTableViewCell.cellIdentifier)
+            $0.isScrollEnabled = false
+            $0.separatorStyle = .none
+        }
+    }
+    
+    private func setHierarchy() {
+        self.addSubview(dropdownTableView)
+    }
+    
+    private func setLayout() {
+        dropdownTableView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    //MARK: - Custom Method
+    
+    private func setDropDown() {
+        dropdownData
+            .bind(to: dropdownTableView.rx.items(cellIdentifier: WSSDropdownTableViewCell.cellIdentifier, cellType: WSSDropdownTableViewCell.self)) { row, text, cell in
+            cell.bindText(text: text)
+        }
+        .disposed(by: disposeBag)
+        
+        //cellItem 넘겨주는 함수
+        dropdownTableView.rx.modelSelected(String.self)
+            .subscribe(onNext: { [weak self] cell in
+            print(cell)
+        })
+        .disposed(by: disposeBag)
+    }
+}
+
---
+++ WSSiOS/WSSiOS/Source/Presentation/Base/WSSDropdownManager.swift
@@ -0,0 +1,8 @@
+//
+//  WSSDropdownManager.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 4/1/24.
+//
+
+import Foundation
