e43efdbf389921b1b1bf44abe52f70933d1607e9
shinzz410 <tlswldnjs410@gmail.com>
2024-04-01 15:35:37 +0900

    [Design] #139 - DropDownTableViewCell 구현

---
+++ WSSiOS/WSSiOS/Source/Presentation/Base/WSSDropdownTableViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  WSSDropdownTableViewCell.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 4/1/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class WSSDropdownTableViewCell: UITableViewCell {
+
+    //MARK: - Components
+    
+    private let cellLabel = UILabel()
+    private let divderView = UIView()
+    
+    // MARK: - Life Cycles
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI
+
+    private func setUI() {
+        cellLabel.do {
+            $0.font = .Body2
+            $0.textColor = .wssBlack
+        }
+        
+        divderView.do {
+            $0.backgroundColor = .Gray50
+        }
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(divderView,
+                         cellLabel)
+    }
+    
+    private func setLayout() {
+        divderView.snp.makeConstraints {
+            $0.top.width.equalToSuperview()
+            $0.height.equalTo(0.7)
+        }
+        
+        cellLabel.snp.makeConstraints {
+            $0.centerY.centerX.equalToSuperview()
+//            $0.leading.trailing.equalToSuperview().inset(27)
+        }
+    }
+    
+    // MARK: - Data
+    
+    func bindText(text: String) {
+        cellLabel.text = text
+    }
+}
