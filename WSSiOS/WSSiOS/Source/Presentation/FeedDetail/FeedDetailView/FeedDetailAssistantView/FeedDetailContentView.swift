7711d910979a858a9c4d13895632fdca839f6d2e
Guryss <lala1470@konkuk.ac.kr>
2024-06-24 00:16:05 +0900

    [Add] #171 - FeedDetailView에 필요한 추가 에셋 적용

---
+++ WSSiOS/WSSiOS/Source/Presentation/FeedDetail/FeedDetailView/FeedDetailAssistantView/FeedDetailContentView.swift
@@ -0,0 +1,8 @@
+//
+//  FeedDetailContentView.swift
+//  WSSiOS
+//
+//  Created by Seoyeon Choi on 6/23/24.
+//
+
+import Foundation
---
+++ WSSiOS/WSSiOS/Source/Presentation/FeedDetail/FeedDetailView/FeedDetailAssistantView/FeedDetailProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  FeedDetailProfileView.swift
+//  WSSiOS
+//
+//  Created by Seoyeon Choi on 6/23/24.
+//
+
+import Foundation
