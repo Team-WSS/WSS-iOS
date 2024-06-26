d07fd45ab43c4d5f6b1d71e546941a0f43c58f1a
Guryss <lala1470@konkuk.ac.kr>
2024-06-26 19:55:09 +0900

    [Feat] #171 - 레포지토리 패턴을 활용하여 더미데이터 추가 및 FeedDetailViewModel 생성

--- WSSiOS/WSSiOS/Source/Data/Repository/FeedRepository.swift
+++ WSSiOS/WSSiOS/Source/Data/Repository/FeedRepository.swift
@@ -13,6 +13,73 @@ protocol FeedRepository {
     func getFeedData(category: String,
                      lastFeedId: Int,
                      size: Int) -> Observable<TotalFeed>
+    
+    func getSingleFeedData() -> Observable<Feed>
+    func getSingleFeedComments() -> Observable<[Comment]>
+}
+
+struct TestFeedRepository: FeedRepository {
+    func getFeedData(category: String, lastFeedId: Int, size: Int) -> Observable<TotalFeed> {
+        return Observable.just(TotalFeed(category: "drama", isLoadable: true, feeds: [TotalFeeds(userId: 123,
+                                                                                                 nickname: "지원입니둥",
+                                                                                                 avatarImage: dummyFeedImage,
+                                                                                                 feedId: 846,
+                                                                                                 createdDate: "3월 21일",
+                                                                                                 feedContent: "판소추천해요!",
+                                                                                                 likeCount: 333,
+                                                                                                 isLiked: false,
+                                                                                                 commentCount: 7,
+                                                                                                 novelId: 123,
+                                                                                                 title: "안녕하세요전안안녕한데요?안녕하세요?안녕하신지요?",
+                                                                                                 novelRatingCount: 523,
+                                                                                                 novelRating: 4.0,
+                                                                                                 relevantCategories: ["wuxia", "romance", "wuxia", "romance"],
+                                                                                                 isSpolier: false,
+                                                                                                 isModified: true,
+                                                                                                 isMyFeed: false)]))
+    }
+    
+    func getSingleFeedData() -> Observable<Feed> {
+        return Observable.just(Feed(userProfileImage: "imgTest2", 
+                                    userNickName: "구리구리스",
+                                    createdDate: "10월 3일",
+                                    content: "짱짱걸",
+                                    novelTitle: "여주가 세계를 구한다고라고라",
+                                    novelRating: 4.21,
+                                    novelRatingCount: 123,
+                                    genres: [.bl, .drama],
+                                    likeCount: 12,
+                                    commentCount: 23))
+    }
+    
+    func getSingleFeedComments() -> Observable<[Comment]> {
+        return Observable.just(
+            [Comment(userId: 1,
+                     userNickname: "구리스",
+                     userProfileImage: "imgTest2",
+                     commentId: 1,
+                     createdDate: "10월 3일",
+                     commentContent: "진짜 재미있다 ㄷㄷ",
+                     isModified: true,
+                     isMyComment: true),
+             Comment(userId: 1,
+                      userNickname: "이진토",
+                      userProfileImage: "imgTest2",
+                      commentId: 1,
+                      createdDate: "11월 16일",
+                      commentContent: "진짜 더 재미있다 ㄷㄷ",
+                      isModified: true,
+                      isMyComment: true),
+             Comment(userId: 1,
+                      userNickname: "이안",
+                      userProfileImage: "imgTest2",
+                      commentId: 1,
+                      createdDate: "10월 3일",
+                      commentContent: "진짜진짜 재미있다!",
+                      isModified: true,
+                      isMyComment: true)
+            ])
+    }
 }
 
 struct DefaultFeedRepository: FeedRepository {
@@ -25,5 +92,38 @@ struct DefaultFeedRepository: FeedRepository {
     func getFeedData(category: String, lastFeedId: Int, size: Int) -> RxSwift.Observable<TotalFeed> {
         return feedService.getFeedList(category: category, lastFeedId: lastFeedId, size: size)
         .asObservable()
+    }
+    
+    func getSingleFeedData() -> Observable<Feed> {
+        return Observable.just(Feed(userProfileImage: "imgTest2", userNickName: "구리구리스", createdDate: "10월 3일", content: "짱짱걸", novelTitle: "여주가 세계를 구한다고라고라", novelRating: 4.21, novelRatingCount: 123, genres: [.bl, .drama], likeCount: 12, commentCount: 23))
+    }
+    
+    func getSingleFeedComments() -> Observable<[Comment]> {
+        return Observable.just(
+            [Comment(userId: 1,
+                     userNickname: "구리스",
+                     userProfileImage: "imgTest2",
+                     commentId: 1,
+                     createdDate: "10월 3일",
+                     commentContent: "진짜 재미있다 ㄷㄷ",
+                     isModified: true,
+                     isMyComment: true),
+             Comment(userId: 1,
+                      userNickname: "이진토",
+                      userProfileImage: "imgTest2",
+                      commentId: 1,
+                      createdDate: "11월 16일",
+                      commentContent: "진짜 더 재미있다 ㄷㄷ",
+                      isModified: true,
+                      isMyComment: true),
+             Comment(userId: 1,
+                      userNickname: "이안",
+                      userProfileImage: "imgTest2",
+                      commentId: 1,
+                      createdDate: "10월 3일",
+                      commentContent: "진짜진짜 재미있다!",
+                      isModified: true,
+                      isMyComment: true)
+            ])
     }
 }
--- WSSiOS/WSSiOS/Source/Presentation/FeedDetail/FeedDetailViewModel/FeedDetailViewModel.swift
+++ WSSiOS/WSSiOS/Source/Presentation/FeedDetail/FeedDetailViewModel/FeedDetailViewModel.swift
@@ -8,27 +8,47 @@
 import UIKit
 
 import RxSwift
+import RxCocoa
 
 final class FeedDetailViewModel: ViewModelType {
     
     //MARK: - Properties
     
+    private let feedRepository: FeedRepository
+    private let disposeBag = DisposeBag()
     
+    private let feedDetailData = PublishSubject<Feed>()
+    private let commentsDataList = PublishSubject<[Comment]>()
     
     //MARK: - Life Cycle
     
+    init(feedRepository: FeedRepository) {
+        self.feedRepository = feedRepository
+    }
     
     struct Input {
         
     }
     
     struct Output {
-        
+        let feedDetailData: Observable<Feed>
+        let commentDataList: Observable<[Comment]>
     }
     
     func transform(from input: Input, disposeBag: DisposeBag) -> Output {
-        let output = Output()
+        feedRepository.getSingleFeedData()
+            .subscribe(with: self, onNext: { owner, data in
+                owner.feedDetailData.onNext(data)
+            })
+            .disposed(by: disposeBag)
         
-        return output
+        feedRepository.getSingleFeedComments()
+            .subscribe(with: self, onNext: { owner, data in
+                owner.commentsDataList.onNext(data)
+            })
+            .disposed(by: disposeBag)
+
+        return Output(feedDetailData: feedDetailData.asObservable(),
+                      commentDataList: commentsDataList.asObservable())
     }
 }
