127a877e784558590eb3898963e122cbcb0ea1e5
shinzz410 <tlswldnjs410@gmail.com>
2024-07-15 05:31:47 +0900

    [Chore] #184 - SceneDelegate 돌려놓기

--- WSSiOS/WSSiOS/App/SceneDelegate.swift
+++ WSSiOS/WSSiOS/App/SceneDelegate.swift
@@ -14,10 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let navigationController = UINavigationController(rootViewController: MyPageViewController(
-            userRepository: DefaultUserRepository(
-                userService: DefaultUserService()),
-            avatarRepository: DefaultAvatarRepository(avatarService: DefaultAvatarService())))
+        let navigationController = UINavigationController(rootViewController: WSSTabBarController())
         navigationController.isNavigationBarHidden = true
         
         self.window = UIWindow(windowScene: windowScene)
