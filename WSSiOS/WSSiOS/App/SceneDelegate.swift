5afce01bbe11e227f3193f26ba4b32174efc7761
shinzz410 <tlswldnjs410@gmail.com>
2024-07-29 00:01:59 +0900

    [Chore] #main - SceneDelegate 수정

--- WSSiOS/WSSiOS/App/SceneDelegate.swift
+++ WSSiOS/WSSiOS/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let navigationController = UINavigationController(rootViewController: FeedEditViewController(viewModel: FeedEditViewModel(memoRepository: DefaultMemoRepository(memoService: DefaultMemoService()))))
+        let navigationController = UINavigationController(rootViewController: WSSTabBarController())
         navigationController.isNavigationBarHidden = true
         
         self.window = UIWindow(windowScene: windowScene)
