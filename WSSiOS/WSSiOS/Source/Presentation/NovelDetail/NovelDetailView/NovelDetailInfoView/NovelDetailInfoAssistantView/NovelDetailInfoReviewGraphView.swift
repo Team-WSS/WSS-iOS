6e6c651976ea56e5c5e70d7fe4ce501861f088ba
YunHak Lee <lyh990408@gmail.com>
2024-08-16 19:14:05 +0900

    [Chore] #174 - ReviewGraph 에 View 안붙은것 수정

--- WSSiOS/WSSiOS/Source/Presentation/NovelDetail/NovelDetailView/NovelDetailInfoView/NovelDetailInfoAssistantView/NovelDetailInfoReviewGraphView.swift
+++ WSSiOS/WSSiOS/Source/Presentation/NovelDetail/NovelDetailView/NovelDetailInfoView/NovelDetailInfoAssistantView/NovelDetailInfoReviewGraphView.swift
@@ -1,5 +1,5 @@
 //
-//  NovelDetailInfoReviewGraph.swift
+//  NovelDetailInfoReviewGraphView.swift
 //  WSSiOS
 //
 //  Created by YunhakLee on 8/8/24.
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 import Then
 
-final class NovelDetailInfoReviewGraph: UIView {
+final class NovelDetailInfoReviewGraphView: UIView {
     
     let readStatus: ReadStatus
     
