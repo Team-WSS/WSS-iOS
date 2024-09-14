7317c1c84598cd1dbbdfc730d17459d3ff8d8eb5
YunHak Lee <lyh990408@gmail.com>
2024-09-14 21:45:54 +0900

    [Chore] #206 - 테스트용 코드 추가

--- WSSiOS/WSSiOS/App/SceneDelegate.swift
+++ WSSiOS/WSSiOS/App/SceneDelegate.swift
@@ -14,8 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let navigationController = UINavigationController(rootViewController: WSSTabBarController())
-        navigationController.isNavigationBarHidden = true
+        let navigationController = UINavigationController(rootViewController: TestViewController())
+        navigationController.isNavigationBarHidden = false
         
         self.window = UIWindow(windowScene: windowScene)
         self.window?.rootViewController = navigationController
