5675f4bc6ff36f7ad4faed7ce4da96c34cdc056d
YunHak Lee <lyh990408@gmail.com>
2024-09-17 15:09:52 +0900

    [Add] #227 - LoginView 관련 뷰 추가

---
+++ WSSiOS/WSSiOS/Source/Presentation/Onboarding/Login/LoginView/LoginAssistantView/LoginPlatformButton.swift
@@ -0,0 +1,46 @@
+//
+//  LoginPlatformButton.swift
+//  WSSiOS
+//
+//  Created by YunhakLee on 9/17/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class LoginPlatformButton: UIView {
+    
+    //MARK: - Components
+   
+    
+    
+    //MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - UI
+    
+    private func setUI() {
+        
+    }
+    
+    private func setHierarchy() {
+       
+    }
+    
+    private func setLayout() {
+       
+    }
+}
