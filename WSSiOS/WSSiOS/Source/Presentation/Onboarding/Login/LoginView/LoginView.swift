5358303febc47d1f66978d788c51b956bb1c4fee
YunHak Lee <lyh990408@gmail.com>
2024-09-17 16:07:32 +0900

    [Design] #227 - LoginView에 보조 뷰 배치

--- WSSiOS/WSSiOS/Source/Presentation/Onboarding/Login/LoginView/LoginView.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Onboarding/Login/LoginView/LoginView.swift
@@ -14,7 +14,10 @@ final class LoginView: UIView {
     
     //MARK: - Components
    
+    let backgroundImgaeView = UIImageView()
     
+    let platformButtonStackView = LoginPlatformButtonStackView()
+    let skipButton = LoginSkipButton()
     
     //MARK: - Life Cycle
     
@@ -33,14 +36,34 @@ final class LoginView: UIView {
     //MARK: - UI
     
     private func setUI() {
+        self.backgroundColor = .white
         
+        backgroundImgaeView.do {
+            $0.image = .imgLoginBackground
+            $0.contentMode = .scaleAspectFill
+            $0.clipsToBounds = true
+        }
     }
     
     private func setHierarchy() {
-       
+        self.addSubview(backgroundImgaeView)
+        backgroundImgaeView.addSubviews(platformButtonStackView,
+                                        skipButton)
     }
     
     private func setLayout() {
-       
+        backgroundImgaeView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        platformButtonStackView.snp.makeConstraints {
+            $0.bottom.equalTo(skipButton.snp.top).offset(-34)
+            $0.centerX.equalToSuperview()
+        }
+        
+        skipButton.snp.makeConstraints {
+            $0.bottom.equalTo(self.safeAreaLayoutGuide).offset(-10)
+            $0.horizontalEdges.equalToSuperview().inset(16)
+        }
     }
 }
