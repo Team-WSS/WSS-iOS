d1953fb8d2ed0e7b8c14f2b0595ce5e66e0c4c31
YunHak Lee <lyh990408@gmail.com>
2024-09-19 17:18:57 +0900

    [Design] #227 - LoginView 캐러셀뷰 구현, 무한스크롤 구현.

--- WSSiOS/WSSiOS/Source/Presentation/Onboarding/Login/LoginView/LoginAssistantView/LoginCarouselView.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Onboarding/Login/LoginView/LoginAssistantView/LoginCarouselView.swift
@@ -11,10 +11,14 @@ import SnapKit
 import Then
 
 final class LoginCarouselView: UIView {
-    
+
     //MARK: - Components
    
+    let bannerCollectionView = UICollectionView(frame: .zero,
+                                                      collectionViewLayout: UICollectionViewLayout())
+    private let bannerCollectionViewLayout = UICollectionViewFlowLayout()
     
+
     
     //MARK: - Life Cycle
     
@@ -33,14 +37,44 @@ final class LoginCarouselView: UIView {
     //MARK: - UI
     
     private func setUI() {
+        bannerCollectionViewLayout.do {
+            $0.scrollDirection = .horizontal
+            $0.minimumLineSpacing = 0
+            $0.itemSize = CGSize(width: LoginCarouselMetric.collectionViewWidth,
+                                 height:  LoginCarouselMetric.collectionViewHeight)
+           bannerCollectionView.setCollectionViewLayout($0,
+                                                        animated: false)
+        }
         
+        bannerCollectionView.do {
+            $0.isScrollEnabled = true
+            $0.showsHorizontalScrollIndicator = false
+            $0.showsVerticalScrollIndicator = false
+            $0.backgroundColor = .clear
+            $0.isPagingEnabled = true
+        }
+        
+        bannerCollectionView.setContentOffset(CGPoint(x: LoginCarouselMetric.collectionViewWidth,
+                                                      y: 0),
+                                              animated: false)
     }
     
     private func setHierarchy() {
-       
+        self.addSubviews(bannerCollectionView)
     }
     
     private func setLayout() {
-       
+        bannerCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+            $0.height.equalTo(LoginCarouselMetric.collectionViewHeight)
+        }
+    }
+}
+
+enum LoginCarouselMetric {
+    static let imageRatio: CGFloat = 1.368
+    static let collectionViewWidth = UIScreen.main.bounds.width
+    static var collectionViewHeight: CGFloat {
+        return UIScreen.isSE ? 460 : collectionViewWidth * imageRatio
     }
 }
