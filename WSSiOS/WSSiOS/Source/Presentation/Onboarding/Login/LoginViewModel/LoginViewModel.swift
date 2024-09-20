61fd9dacace082bce05e9c9e947e37767b25ac99
YunHak Lee <lyh990408@gmail.com>
2024-09-20 20:42:37 +0900

    [Design] #227 - 2.0초마다 자동 스크롤 구현, 자동 스크롤 일시중지 및 재개 메서드 추가

--- WSSiOS/WSSiOS/Source/Presentation/Onboarding/Login/LoginViewModel/LoginViewModel.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Onboarding/Login/LoginViewModel/LoginViewModel.swift
@@ -15,35 +15,79 @@ final class LoginViewModel: ViewModelType {
     
     //MARK: - Properties
     
-    private let bannerImages = BehaviorRelay<[UIImage]>(value: [UIImage(resource: .imgLoginBanner4),
-                                                                UIImage(resource: .imgLoginBanner1),
-                                                                UIImage(resource: .imgLoginBanner2),
-                                                                UIImage(resource: .imgLoginBanner3),
-                                                                UIImage(resource: .imgLoginBanner4),
-                                                                UIImage(resource: .imgLoginBanner1)])
-
+    private let bannerImages = BehaviorRelay<[UIImage]>(
+        value: [UIImage(resource: .imgLoginBanner4),
+                UIImage(resource: .imgLoginBanner1),
+                UIImage(resource: .imgLoginBanner2),
+                UIImage(resource: .imgLoginBanner3),
+                UIImage(resource: .imgLoginBanner4),
+                UIImage(resource: .imgLoginBanner1)])
+    
+    private let indicatorIndex = BehaviorRelay<Int>(value: 0)
+    private let autoScrollTrigger = PublishRelay<Void>()
+    private var autoScrollDisposable: Disposable?
     
     //MARK: - Life Cycle
     
-    //    init(detailRepository: NovelDetailRepository, novelId: Int = 0) {
-    //        self.novelDetailRepository = detailRepository
-    //        self.novelId = novelId
-    //    }
     
     //MARK: - Transform
     
     struct Input {
-        
+        let bannerCollectionViewContentOffset: ControlProperty<CGPoint>
+        //let manualScrollTrigger: Observable<Void>
     }
     
     struct Output {
         let bannerImages: Driver<[UIImage]>
+        let indicatorIndex: Driver<Int>
+        let autoScrollTrigger: Driver<Void>
     }
     
     func transform(from input: Input, disposeBag: DisposeBag) -> Output {
+        input.bannerCollectionViewContentOffset
+            .bind(with: self, onNext: { owner, value in
+                let scrollOffset = value.x
+                
+                var index = Int((scrollOffset + LoginBannerMetric.width/2.0)/LoginBannerMetric.width) - 1
+                
+                if index == 4 {
+                    index = 0
+                } else if index == -1 {
+                    index = 3
+                }
+                
+                owner.indicatorIndex.accept(index)
+            })
+            .disposed(by: disposeBag)
+        
+        let autoScrollTrigger = self.autoScrollTrigger
+            .asDriver(onErrorJustReturn: ())
         
         return Output(
-            bannerImages: bannerImages.asDriver()
+            bannerImages: bannerImages.asDriver(),
+            indicatorIndex: indicatorIndex.asDriver(),
+            autoScrollTrigger: autoScrollTrigger
         )
     }
+    
+    //MARK: - Custom Method
+    
+    func pauseAutoScroll() {
+        autoScrollDisposable?.dispose()
+    }
+    
+    func resumeAutoScroll() {
+        autoScrollDisposable?.dispose()
+        autoScrollDisposable = Observable<Int>.interval(.milliseconds(2000), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] _ in
+                self?.autoScrollTrigger.accept(())
+            })
+    }
 }
+
+//0.0 - 0.5 -> 3
+//0.5 - 1.5 -> 0
+//1.5 - 2.5 -> 1
+//2.5 - 3.5 -> 2
+//3.5 - 4.5 -> 3
+//4.5 - 5.0 -> 0
