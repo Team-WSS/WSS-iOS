b1e299c0e730df20b0ef9042ea6b27996571b32c
shinzz410 <tlswldnjs410@gmail.com>
2024-09-20 10:14:50 +0900

    [Chore] #239 - SceneDelegate 복귀

--- WSSiOS/WSSiOS/App/SceneDelegate.swift
+++ WSSiOS/WSSiOS/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let navigationController = UINavigationController(rootViewController: MyPageDeleteIDWarningViewController(userRepository: DefaultUserRepository(userService: DefaultUserService(), blocksService: DefaultBlocksService())))
+        let navigationController = UINavigationController(rootViewController: WSSTabBarController())
         navigationController.isNavigationBarHidden = true
         
         self.window = UIWindow(windowScene: windowScene)
