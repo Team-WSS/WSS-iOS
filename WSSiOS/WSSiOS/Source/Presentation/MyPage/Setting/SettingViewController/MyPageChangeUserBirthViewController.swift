df9b7a775cda7ea4361374ca5d88da28c975cc99
shinzz410 <tlswldnjs410@gmail.com>
2024-09-23 16:34:16 +0900

    [Add] #29 - MyPageChangeUserBirth 파일 추가

---
+++ WSSiOS/WSSiOS/Source/Presentation/MyPage/Setting/SettingViewController/MyPageChangeUserBirthViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MyPageChangeUserBirthViewController.swift
+//  WSSiOS
+//
+//  Created by 신지원 on 9/23/24.
+//
+
+import UIKit
+
+import RxSwift
+
+final class MyPageChangeUserBirthViewController: UIViewController {
+    
+    //MARK: - Properties
+
+    private let disposeBag = DisposeBag()
+    
+    //MARK: - Components
+    
+    private let rootView = MyPageDeleteIDView()
+    
+    // MARK: - Life Cycle
+    
+    override func loadView() {
+        self.view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+    
+    //MARK: - Bind
+    
+
+}
