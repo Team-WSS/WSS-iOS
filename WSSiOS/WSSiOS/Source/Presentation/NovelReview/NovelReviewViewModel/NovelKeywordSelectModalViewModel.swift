f6c9f8fb8df2addeb4cd2aa004b5afdc3fb34b37
Hyowon Jeon <hyowon612@gmail.com>
2024-09-24 15:57:05 +0900

    [Feat] #211 - NovelKeywordSelectModalViewModel Output Observable로 변경

--- WSSiOS/WSSiOS/Source/Presentation/NovelReview/NovelReviewViewModel/NovelKeywordSelectModalViewModel.swift
+++ WSSiOS/WSSiOS/Source/Presentation/NovelReview/NovelReviewViewModel/NovelKeywordSelectModalViewModel.swift
@@ -14,11 +14,19 @@ final class NovelKeywordSelectModalViewModel: ViewModelType {
     
     //MARK: - Properties
     
-    var enteredText: String = ""
     var keywordSearchResultList: [String] = ["환생생", "환생남주", "환생이", "환생물", "환생", "환환생", "환생여주", "환환환생", "후회", "정치물", "피폐", "빙의", "먼치킨", "기억상실", "가가", "나나", "다다다", "라라랄", "마마마마마", "밥", "사삿", "아아앙", "자자", "차촟", "카카카", "타톹", "파포", "하히히"]
     var selectedKeywordList: [String]
     
     private let keywordLimit: Int = 20
+    
+    // Output
+    
+    private let dismissModalViewController = PublishRelay<Void>()
+    private let enteredText = BehaviorRelay<String>(value: "")
+    private let selectedKeywordListData = BehaviorRelay<[String]>(value: [])
+    private let keywordSearchResultListData = BehaviorRelay<[String]>(value: [])
+    private let searchResultCollectionViewHeight = BehaviorRelay<CGFloat>(value: 0)
+    private let isKeywordCountOverLimit = PublishRelay<IndexPath>()
     
     //MARK: - Life Cycle
     
@@ -41,95 +49,96 @@ final class NovelKeywordSelectModalViewModel: ViewModelType {
     }
     
     struct Output {
-        let dismissModalViewController = PublishRelay<Void>()
-        let enteredText = PublishRelay<String>()
-        let selectedKeywordListData = PublishRelay<[String]>()
-        let keywordSearchResultListData = BehaviorRelay<[String]>(value: [])
-        let searchResultCollectionViewHeight = BehaviorRelay<CGFloat>(value: 0)
-        let isKeywordCountOverLimit = PublishRelay<IndexPath>()
+        let dismissModalViewController: Observable<Void>
+        let enteredText: Observable<String>
+        let selectedKeywordListData: Observable<[String]>
+        let keywordSearchResultListData: Observable<[String]>
+        let searchResultCollectionViewHeight: Observable<CGFloat>
+        let isKeywordCountOverLimit: Observable<IndexPath>
     }
     
     func transform(from input: Input, disposeBag: DisposeBag) -> Output {
-        let output = Output()
-        
         input.viewDidLoadEvent
             .subscribe(with: self, onNext: { owner, _ in
-                output.selectedKeywordListData.accept(owner.selectedKeywordList)
+                owner.selectedKeywordListData.accept(owner.selectedKeywordList)
             })
             .disposed(by: disposeBag)
         
         input.updatedEnteredText
             .subscribe(with: self, onNext: { owner, text in
-                owner.enteredText = text
-                output.enteredText.accept(owner.enteredText)
+                owner.enteredText.accept(text)
              })
             .disposed(by: disposeBag)
         
         input.searchCancelButtonDidTap
             .subscribe(with: self, onNext: { owner, _ in
-                owner.enteredText = ""
-                output.enteredText.accept(owner.enteredText)
+                owner.enteredText.accept("")
             })
             .disposed(by: disposeBag)
         
         input.closeButtonDidTap
-            .subscribe(onNext: { _ in
-                output.dismissModalViewController.accept(())
+            .subscribe(with: self, onNext: { owner, _ in
+                owner.dismissModalViewController.accept(())
             })
             .disposed(by: disposeBag)
         
         input.searchButtonDidTap
             .subscribe(with: self, onNext: { owner, _ in
-                output.keywordSearchResultListData.accept(owner.keywordSearchResultList)
+                owner.keywordSearchResultListData.accept(owner.keywordSearchResultList)
             })
             .disposed(by: disposeBag)
         
         input.selectedKeywordCollectionViewItemSelected
             .subscribe(with: self, onNext: { owner, indexPath in
                 owner.selectedKeywordList.remove(at: indexPath.item)
-                output.selectedKeywordListData.accept(owner.selectedKeywordList)
-                output.keywordSearchResultListData.accept(owner.keywordSearchResultList)
+                owner.selectedKeywordListData.accept(owner.selectedKeywordList)
+                owner.keywordSearchResultListData.accept(owner.keywordSearchResultList)
             })
             .disposed(by: disposeBag)
         
         input.searchResultCollectionViewContentSize
             .map { $0?.height ?? 0 }
-            .bind(to: output.searchResultCollectionViewHeight)
+            .bind(to: self.searchResultCollectionViewHeight)
             .disposed(by: disposeBag)
         
         input.searchResultCollectionViewItemSelected
             .subscribe(with: self, onNext: { owner, indexPath in
                 if owner.selectedKeywordList.count >= owner.keywordLimit {
-                    output.isKeywordCountOverLimit.accept(indexPath)
+                    owner.isKeywordCountOverLimit.accept(indexPath)
                 } else {
                     owner.selectedKeywordList.append(owner.keywordSearchResultList[indexPath.item])
                 }
-                output.selectedKeywordListData.accept(owner.selectedKeywordList)
+                owner.selectedKeywordListData.accept(owner.selectedKeywordList)
             })
             .disposed(by: disposeBag)
         
         input.searchResultCollectionViewItemDeselected
             .subscribe(with: self, onNext: { owner, indexPath in
                 owner.selectedKeywordList.removeAll { $0 == owner.keywordSearchResultList[indexPath.item] }
-                output.selectedKeywordListData.accept(owner.selectedKeywordList)
+                owner.selectedKeywordListData.accept(owner.selectedKeywordList)
             })
             .disposed(by: disposeBag)
         
         input.resetButtonDidTap
             .subscribe(with: self, onNext: { owner, _ in
                 owner.selectedKeywordList = []
-                output.selectedKeywordListData.accept(owner.selectedKeywordList)
-                output.keywordSearchResultListData.accept(owner.keywordSearchResultList)
+                owner.selectedKeywordListData.accept(owner.selectedKeywordList)
+                owner.keywordSearchResultListData.accept(owner.keywordSearchResultList)
             })
             .disposed(by: disposeBag)
         
         input.selectButtonDidTap
             .subscribe(with: self, onNext: { owner, _ in
                 NotificationCenter.default.post(name: NSNotification.Name("NovelReviewKeywordSelected"), object: owner.selectedKeywordList)
-                output.dismissModalViewController.accept(())
+                owner.dismissModalViewController.accept(())
             })
             .disposed(by: disposeBag)
         
-        return output
+        return Output(dismissModalViewController: dismissModalViewController.asObservable(),
+                      enteredText: enteredText.asObservable(),
+                      selectedKeywordListData: selectedKeywordListData.asObservable(),
+                      keywordSearchResultListData: keywordSearchResultListData.asObservable(),
+                      searchResultCollectionViewHeight: searchResultCollectionViewHeight.asObservable(),
+                      isKeywordCountOverLimit: isKeywordCountOverLimit.asObservable())
     }
 }
