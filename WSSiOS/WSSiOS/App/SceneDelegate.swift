ba3d212a6300969057bb221db0a598114e4da118
shinzz410 <tlswldnjs410@gmail.com>
2024-09-26 04:37:30 +0900

    [Fix] #main - rootVC 복구

--- WSSiOS/WSSiOS/App/SceneDelegate.swift
+++ WSSiOS/WSSiOS/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let navigationController = UINavigationController(rootViewController: MyPageChangeUserInfoViewController(viewModel: MyPageChangeUserInfoViewModel(userRepository: DefaultUserRepository(userService: DefaultUserService(), blocksService: DefaultBlocksService()), userInfo: ChangeUserInfo(gender: "F", birth: 2000))))
+        let navigationController = UINavigationController(rootViewController: WSSTabBarController())
         navigationController.isNavigationBarHidden = true
         
         self.window = UIWindow(windowScene: windowScene)
