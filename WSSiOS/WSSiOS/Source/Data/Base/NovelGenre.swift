35faee215d26ca65d4f7020cf420270eb4afc20e
YunHak Lee <lyh990408@gmail.com>
2024-09-26 16:02:53 +0900

    [Add] #220 - NewNovelGenre Enum 추가 및 한국어로 장르를 초기화할 수 있는 메서드 추가

--- WSSiOS/WSSiOS/Source/Data/Base/NovelGenre.swift
+++ WSSiOS/WSSiOS/Source/Data/Base/NovelGenre.swift
@@ -234,3 +234,80 @@ enum FeedDetailManKoreanGenre: String, CaseIterable {
         }
     }
 }
+
+enum NewNovelGenre: String, CaseIterable {
+    case all = "all"
+    case fantasy = "fantasy"
+    case modernFantasy = "modernFantasy"
+    case wuxia = "wuxia"
+    case drama = "drama"
+    case mystery = "mystery"
+    case lightNovel = "lightNovel"
+    case romance = "romance"
+    case romanceFantasy = "romanceFantasy"
+    case bl = "BL"
+    case etc = "etc"
+    case error = "error"
+    
+    var withKorean: String {
+        switch self {
+        case .all:
+            return "전체"
+        case .fantasy:
+            return "판타지"
+        case .modernFantasy:
+            return "현판"
+        case .wuxia:
+            return "무협"
+        case .drama:
+            return "드라마"
+        case .mystery:
+            return "미스터리"
+        case .lightNovel:
+            return "라노벨"
+        case .romance:
+            return "로맨스"
+        case .romanceFantasy:
+            return "로판"
+        case .bl:
+            return "BL"
+        case .etc:
+            return "기타"
+        case .error:
+            return "error"
+        }
+    }
+    
+    static func withKoreanRawValue(from genre: String) -> NewNovelGenre {
+        switch genre {
+        case "전체":
+            return .all
+        case "판타지":
+            return .fantasy
+        case "현판":
+            return .modernFantasy
+        case "무협":
+            return .wuxia
+        case  "드라마":
+            return .drama
+        case "미스터리":
+            return .mystery
+        case "라노벨":
+            return .lightNovel
+        case "로맨스":
+            return .romance
+        case "로판":
+            return .romanceFantasy
+        case  "BL":
+            return .bl
+        case "기타":
+            return .etc
+        default :
+            return .error
+        }
+    }
+}
+
+extension NewNovelGenre {
+    static let feedEditGenres: [NewNovelGenre] = [.fantasy, .modernFantasy, .romance, .romanceFantasy, .wuxia, .drama, .mystery, .lightNovel, .bl, .etc]
+}
