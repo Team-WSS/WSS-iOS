80dc56e7b0d5612929445f2fcdcb0eabcb1f2a55
shinzz410 <tlswldnjs410@gmail.com>
2024-09-28 03:06:55 +0900

    [Fix] #259 - DropDown 비동기 처리

--- WSSiOS/WSSiOS/Source/Presentation/Base/Dropdown/WSSDropdownManager.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Base/Dropdown/WSSDropdownManager.swift
@@ -28,20 +28,26 @@ class WSSDropdownManager {
                         dropdownData: [String],
                         textColor: UIColor) {
         
-        let dropdownTableView = WSSDropdownTableView()
-        dropdownTableView.dropdownData.onNext(dropdownData)
-        dropdownTableView.isHidden = true
-        dropdownTableView.cellTextColor = textColor
-
+        let dropdownTableView = WSSDropdownTableView().then {
+            $0.dropdownData.onNext(dropdownData)
+            $0.isHidden = true
+            $0.cellTextColor = textColor
+        }
+        
         dropdownRootView.addSubview(dropdownTableView)
         
-        dropdownTableView.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(100)
-            $0.trailing.equalToSuperview().inset(20.5)
-            $0.width.equalTo(dropdownWidth)
-            
-            let calculateHeight = CGFloat(dropdownData.count) * 51.0
-            $0.height.equalTo(calculateHeight)
+        DispatchQueue.main.async {
+            dropdownTableView.snp.makeConstraints {
+                if let window = dropdownButton.window {
+                    let buttonFrame = dropdownButton.convert(dropdownButton.bounds, to: window)
+                    $0.top.equalTo(dropdownRootView.snp.top).offset(buttonFrame.maxY + 0.5)
+                }
+                $0.trailing.equalToSuperview().inset(20.5)
+                $0.width.equalTo(dropdownWidth)
+                
+                let calculateHeight = CGFloat(dropdownData.count) * 51.0
+                $0.height.equalTo(calculateHeight)
+            }
         }
         
         let tapGesture = UITapGestureRecognizer(target: self,
