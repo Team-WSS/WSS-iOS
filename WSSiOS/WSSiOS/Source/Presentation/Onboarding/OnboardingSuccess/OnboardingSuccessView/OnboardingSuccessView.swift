905ceb75756e22cafd7402b0fb4e70cc862482a7
YunHak Lee <lyh990408@gmail.com>
2024-10-04 16:40:30 +0900

    [Design] #288-3 - VC 기본 세팅, View 바텀 버튼 추가

--- WSSiOS/WSSiOS/Source/Presentation/Onboarding/OnboardingSuccess/OnboardingSuccessView/OnboardingSuccessView.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Onboarding/OnboardingSuccess/OnboardingSuccessView/OnboardingSuccessView.swift
@@ -5,4 +5,49 @@
 //  Created by YunhakLee on 10/4/24.
 //
 
-import Foundation
+import UIKit
+
+import Lottie
+import SnapKit
+import Then
+
+final class OnboardingSuccessView: UIView {
+    
+    //MARK: - Components
+    
+    let completeButton = OnboardingBottomButtonView()
+    
+    //MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - UI
+    
+    private func setUI() {
+        completeButton.do {
+            $0.setText(text: StringLiterals.Onboarding.Success.completeButton)
+        }
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(completeButton)
+    }
+    
+    private func setLayout() {
+        completeButton.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.bottom.equalTo(self.safeAreaLayoutGuide)
+        }
+    }
+}
+
--- WSSiOS/WSSiOS/Source/Presentation/Onboarding/OnboardingSuccess/OnboardingSuccessViewController/OnboardingSuccessViewController.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Onboarding/OnboardingSuccess/OnboardingSuccessViewController/OnboardingSuccessViewController.swift
@@ -5,4 +5,34 @@
 //  Created by YunhakLee on 10/4/24.
 //
 
-import Foundation
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+ 
+final class OnboardingSuccessViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    private let rootView = OnboardingSuccessView()
+    private let disposeBag = DisposeBag()
+    
+    //MARK: - Life Cycle
+    
+    override func loadView() {
+        self.view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+}
