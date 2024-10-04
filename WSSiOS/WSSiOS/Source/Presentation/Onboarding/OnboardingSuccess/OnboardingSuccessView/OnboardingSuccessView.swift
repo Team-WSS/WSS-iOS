a2daf0697f8bd463ada095a1f5bdfedb5f275037
YunHak Lee <lyh990408@gmail.com>
2024-10-04 16:20:01 +0900

    [Design] #288-3 - OnboardingSuccess UI를 위한 기초 세팅

---
+++ WSSiOS/WSSiOS/Source/Presentation/Onboarding/OnboardingSuccess/OnboardingSuccessView/OnboardingSuccessView.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingSuccessView.swift
+//  WSSiOS
+//
+//  Created by YunhakLee on 10/4/24.
+//
+
+import Foundation
---
+++ WSSiOS/WSSiOS/Source/Presentation/Onboarding/OnboardingSuccess/OnboardingSuccessViewController/OnboardingSuccessViewController.swift
@@ -0,0 +1,8 @@
+//
+//  OnboardingSuccessViewController.swift
+//  WSSiOS
+//
+//  Created by YunhakLee on 10/4/24.
+//
+
+import Foundation
