731d6306169a3d909959ac766c376dcbe60013fa
Guryss <lala1470@konkuk.ac.kr>
2024-10-24 01:34:48 +0900

    [Feat] #294 - 상세탐색 결과뷰 내 컬렉션뷰 UI 구현 및 바인딩

--- WSSiOS/WSSiOS/Source/Presentation/Search/DetailSearch/DetailSearchViewController/DetailSearchResultViewController.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Search/DetailSearch/DetailSearchViewController/DetailSearchResultViewController.swift
@@ -32,6 +32,52 @@ final class DetailSearchResultViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        registerCell()
+        setDelegate()
+        
+        bindViewModel()
     }
     
+    private func registerCell() {
+        rootView.novelView.resultNovelCollectionView.register(HomeTasteRecommendCollectionViewCell.self,
+                                                              forCellWithReuseIdentifier: HomeTasteRecommendCollectionViewCell.cellIdentifier)
+    }
+    
+    private func setDelegate() {
+        rootView.novelView
+            .resultNovelCollectionView.rx
+            .setDelegate(self)
+            .disposed(by: disposeBag)
+    }
+    
+    //MARK: - Bind
+    
+    private func bindViewModel() {
+        let input = DetailSearchResultViewModel.Input(
+            backButtonDidTap: rootView.headerView.backButton.rx.tap,
+            novelCollectionViewContentSize: rootView.novelView.resultNovelCollectionView.rx.observe(CGSize.self, "contentSize")
+        )
+        
+        let output = viewModel.transform(from: input, disposeBag: disposeBag)
+        
+        output.popViewController
+            .bind(with: self, onNext: { owner, _ in
+                owner.popToLastViewController()
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.getNovelsObservable()
+            .bind(to: rootView.novelView.resultNovelCollectionView.rx.items(cellIdentifier: HomeTasteRecommendCollectionViewCell.cellIdentifier, cellType: HomeTasteRecommendCollectionViewCell.self)) { row, element, cell in
+                cell.bindData(data: element)
+                print(element.novelTitle)
+            }
+            .disposed(by: disposeBag)
+        
+        output.novelCollectionViewHeight
+            .subscribe(with: self, onNext: { owner, height in
+                owner.rootView.novelView.updateCollectionViewHeight(height: height)
+            })
+            .disposed(by: disposeBag)
+    }
 }
