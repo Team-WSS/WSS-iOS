79b117d446fcfe187925f829409fff8220d878e7
Guryss <lala1470@konkuk.ac.kr>
2024-10-23 22:59:09 +0900

    [Design] #294 - 상세탐색 결과뷰 내 headerView UI 구현

---
+++ WSSiOS/WSSiOS/Source/Presentation/Search/DetailSearch/DetailSearchView/DetailSearchResultView.swift
@@ -0,0 +1,48 @@
+//
+//  DetailSearchResultView.swift
+//  WSSiOS
+//
+//  Created by Seoyeon Choi on 10/23/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class DetailSearchResultView: UIView {
+    
+    //MARK: - UI Components
+    
+    private let headerView = DetailSearchResultHeaderView()
+    
+    //MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        self.backgroundColor = .wssWhite
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(headerView)
+    }
+    
+    private func setLayout() {
+        headerView.snp.makeConstraints {
+            $0.top.equalTo(self.safeAreaLayoutGuide.snp.top)
+            $0.horizontalEdges.equalToSuperview()
+        }
+    }
+}
