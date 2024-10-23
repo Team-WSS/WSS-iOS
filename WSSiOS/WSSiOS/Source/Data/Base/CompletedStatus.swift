3f31f36fd87370e002a3ba8778e4f55934eb26e8
Guryss <lala1470@konkuk.ac.kr>
2024-10-23 16:37:35 +0900

    [Feat] #294 - CompletedStatus로 연재상태 관리 및 커스텀 버튼 클래스 생성

---
+++ WSSiOS/WSSiOS/Source/Data/Base/CompletedStatus.swift
@@ -0,0 +1,27 @@
+//
+//  CompletedStatus.swift
+//  WSSiOS
+//
+//  Created by Seoyeon Choi on 10/23/24.
+//
+
+import Foundation
+
+enum CompletedStatus: String, CaseIterable {
+    case completed
+    case notCompleted
+    
+    var description: String {
+        switch self {
+        case .completed: return "완결작"
+        case .notCompleted: return "연재중"
+        }
+    }
+    
+    var isCompleted: Bool {
+        switch self {
+        case .completed: return true
+        case .notCompleted: return false
+        }
+    }
+}
