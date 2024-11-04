d68f617b61baadb33ab2805f35596af3fcae1284
YunHak Lee <lyh990408@gmail.com>
2024-11-04 13:49:21 +0900

    [Design] #307 - 로딩뷰 파일 추가

---
+++ WSSiOS/WSSiOS/Source/Presentation/Base/NetworkView/WSSLoadingView.swift
@@ -0,0 +1,46 @@
+//
+//  WSSLoadingView.swift
+//  WSSiOS
+//
+//  Created by YunhakLee on 11/4/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class WSSLoadingView: UIView {
+    
+    // MARK: - UI Components
+    
+    private let 
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Custom Method
+    
+    private func setUI() {
+        
+    }
+    
+    private func setHierarchy() {
+       
+    }
+    
+    private func setLayout() {
+        
+    }
+}
