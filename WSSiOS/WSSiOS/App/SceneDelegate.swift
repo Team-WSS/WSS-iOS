fa8c7f9978c0f47d81e5ca7bdcb45ee5cb8d6075
YunHak Lee <lyh990408@gmail.com>
2024-11-15 01:13:18 +0900

    [Add] #329 - 카카오톡으로 로그인을 위한 설정

--- WSSiOS/WSSiOS/App/SceneDelegate.swift
+++ WSSiOS/WSSiOS/App/SceneDelegate.swift
@@ -6,10 +6,20 @@
 //
 
 import UIKit
+import RxKakaoSDKAuth
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.rx.handleOpenUrl(url: url)
+            }
+        }
+    }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
