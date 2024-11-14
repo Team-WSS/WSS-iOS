e0b72bdd38f5e76af397dd4568be7e5b6f292275
YunHak Lee <lyh990408@gmail.com>
2024-11-15 01:09:09 +0900

    [Add] #329 - 카카오 SDK 초기화 코드 추가

--- WSSiOS/WSSiOS/App/AppDelegate.swift
+++ WSSiOS/WSSiOS/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import RxKakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        RxKakaoSDK.initSDK(appKey: "${NATIVE_APP_KEY}")
         return true
     }
 
