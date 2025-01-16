e207367e6452250925cc11ce2130ab622d57d278
YunHak Lee <lyh990408@gmail.com>
2025-01-16 17:25:55 +0900

    [Add] #434 - 파이어베이스 초기화 코드 추가

--- WSSiOS/WSSiOS/App/AppDelegate.swift
+++ WSSiOS/WSSiOS/App/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import RxKakaoSDKCommon
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         RxKakaoSDK.initSDK(appKey: APIConstants.kakaoAppKey)
+        FirebaseApp.configure()
         return true
     }
 
