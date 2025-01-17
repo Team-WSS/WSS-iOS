13308797f53e654834e7e9b1e530f4992be7bbd3
YunHak Lee <lyh990408@gmail.com>
2025-01-18 04:06:51 +0900

    [Feat] #434 - Swizzling 해제를 위한 토큰 직접 등록 구현

--- WSSiOS/WSSiOS/App/AppDelegate.swift
+++ WSSiOS/WSSiOS/App/AppDelegate.swift
@@ -7,30 +7,32 @@
 
 import UIKit
 
-import RxKakaoSDKCommon
+import Firebase
 import FirebaseCore
+import RxKakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         RxKakaoSDK.initSDK(appKey: APIConstants.kakaoAppKey)
-        FirebaseApp.configure()
         
+        FirebaseApp.configure()
         Task {
-            await NotificationHelper.shared.requestAuthorization(application: application)
+            await setFCM(application)
         }
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
@@ -41,15 +43,80 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 // MARK: Notification
 extension AppDelegate {
     
-    // application.registerForRemoteNotifications() 콜백 메서드
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print("디바이스 토큰 등록 성공: \(deviceToken)")
+    @MainActor
+    func setFCM(_ application: UIApplication) async {
+        setDelegate()
+        if await requestNotificationAuthorization() {
+            print("알림 권한 허용")
+            registerForAPNs(application)
+        } else {
+            print("알림 권한 거부")
+        }
     }
-
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    
+    // 알림 및 cloudMessaging Delegate 지정
+    func setDelegate() {
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+    }
+    
+    // 알림 권한 요청
+    func requestNotificationAuthorization() async -> Bool {
+        do {
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            return try await UNUserNotificationCenter.current().requestAuthorization(options: authOptions)
+        } catch {
+            return false
+        }
+    }
+    
+    // APNs에 기기 등록
+    func registerForAPNs(_ application: UIApplication) {
+        application.registerForRemoteNotifications()
+    }
+    
+    // APNs 기기 등록 성공 콜백, Swizzling Off로 직접 device토큰을 Messaging에 등록
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        print("디바이스 토큰 등록 성공: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // APNs 기기 등록 실패 콜백
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error
+    ) {
         print("디바이스 토큰 등록 실패: \(error)")
     }
     
-    
+    //
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        Messaging.messaging().appDidReceiveMessage(userInfo)
+        completionHandler(.noData)
+    }
 }
 
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let fcmToken = fcmToken else { return }
+        print("Firebase 등록 토큰: \(fcmToken)")
+        
+        // 필요 시 서버로 토큰 전송
+        sendFCMTokenToServer(token: fcmToken)
+    }
+
+    private func sendFCMTokenToServer(token: String) {
+        // 서버로 토큰 전송 로직 구현
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+}
--- WSSiOS/WSSiOS/Resource/Helper/NotificationHelper.swift
+++ WSSiOS/WSSiOS/Resource/Helper/NotificationHelper.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-import UserNotifications
+import Firebase
 
 final class NotificationHelper: NSObject, UNUserNotificationCenterDelegate {
     static let shared = NotificationHelper()
     private override init() {
         super.init()
         UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
     }
     
     // 알림 권한 요청 및 APNs에 Notifications 등록(메인 스레드)
@@ -35,5 +36,36 @@ final class NotificationHelper: NSObject, UNUserNotificationCenterDelegate {
         }
     }
     
-    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+      withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+      let userInfo = notification.request.content.userInfo
+
+      Messaging.messaging().appDidReceiveMessage(userInfo)
+
+      // Change this to your preferred presentation option
+      completionHandler([[.alert, .sound]])
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+      let userInfo = response.notification.request.content.userInfo
+
+      Messaging.messaging().appDidReceiveMessage(userInfo)
+
+      completionHandler()
+    }
+}
+
+
+extension NotificationHelper: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("🟢", #function, fcmToken)
+            guard let fcmToken = fcmToken else {
+                print("FCM 등록 토큰을 가져오지 못했습니다.")
+                return
+            }
+            print("FCM 등록 토큰: \(fcmToken)")
+        }
 }
