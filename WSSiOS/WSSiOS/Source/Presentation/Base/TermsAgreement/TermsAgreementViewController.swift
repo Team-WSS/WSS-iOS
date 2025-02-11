059987928854349a7eedd90b6a239716b78ef435
YunHak Lee <lyh990408@gmail.com>
2025-02-11 19:13:46 +0900

    [Add] #473 - 약관동의뷰 UI 구현 준비

---
+++ WSSiOS/WSSiOS/Source/Presentation/Base/TermsAgreement/TermsAgreementViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TermsAgreementViewController.swift
+//  WSSiOS
+//
+//  Created by YunhakLee on 2/11/25.
+//
+
+import UIKit
+
+import RxSwift
+
+class TermsAgreementViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    private let disposeBag = DisposeBag()
+    
+    //MARK: - Components
+    
+    private let rootView = TermsAgreementView()
+    
+    // MARK: - Life Cycle
+    
+    override func loadView() {
+        self.view = rootView
+    }
+}
