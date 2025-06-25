303155c15577f197f76c2b8d10880b8bc181d16f
YunHak Lee <lyh990408@gmail.com>
2025-06-26 02:38:51 +0900

    [Design] #580 - 서재, 피드 공용 정렬 버튼 추가

---
+++ WSSiOS/WSSiOS/Source/Presentation/Base/WSSSortButton.swift
@@ -0,0 +1,79 @@
+//
+//  WSSSortButton.swift
+//  WSSiOS
+//
+//  Created by YunhakLee on 6/26/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class WSSSortButton: UIButton {
+    
+    //MARK: - Components
+    
+    let sortButtonLabel = UILabel()
+    let sortButtonImageView = UIImageView()
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - UI
+    
+    private func setUI() {
+        self.backgroundColor = .wssWhite
+        
+        sortButtonLabel.do {
+            $0.textColor = .wssGray300
+            $0.isUserInteractionEnabled = false
+        }
+        
+        sortButtonImageView.do {
+            $0.image = .icSwitch
+            $0.isUserInteractionEnabled = false
+        }
+    }
+    
+    private func setHierarchy() {
+        self.addSubviews(sortButtonLabel,
+                         sortButtonImageView)
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.height.equalTo(33)
+        }
+        
+        sortButtonImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.size.equalTo(16)
+        }
+        
+        sortButtonLabel.snp.makeConstraints {
+            $0.leading.equalTo(sortButtonImageView.snp.trailing).offset(4)
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+    }
+    
+    //MARK: - Custom Method
+    
+    func updateSortButton(sortType: SortType) {
+        sortButtonLabel.applyWSSFont(.body3, with: sortType.text)
+    }
+}
+
