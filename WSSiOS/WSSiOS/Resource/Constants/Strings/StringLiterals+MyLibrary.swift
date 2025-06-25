02294d647d495533bf4bdcfd6164c8d9dc57c1b8
YunHak Lee <lyh990408@gmail.com>
2025-06-26 02:32:42 +0900

    [Design] #580 - 서재 상단 필터 UI 구현

---
+++ WSSiOS/WSSiOS/Resource/Constants/Strings/StringLiterals+MyLibrary.swift
@@ -0,0 +1,17 @@
+//
+//  StringLiterals+MyLibrary.swift
+//  WSSiOS
+//
+//  Created by YunhakLee on 6/25/25.
+//
+
+extension StringLiterals {
+    enum MyLibrary {
+        enum FilterButton {
+            static let interest = "관심"
+            static let readStatus = "읽기상태"
+            static let starRating = "별점"
+            static let attractivePoint = "매력포인트"
+        }
+    }
+}
