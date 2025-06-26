64a98d484f931ba120c4ba4190b624a8b138faf6
Hyowon Jeon <hyowon612@gmail.com>
2025-06-27 01:47:39 +0900

    [Design] #582 - LibraryCollectionView UI 구현

---
+++ WSSiOS/WSSiOS/Source/Presentation/Library/LibraryView/LibraryCollectionView.swift
@@ -0,0 +1,60 @@
+//
+//  LibraryCollectionView.swift
+//  WSSiOS
+//
+//  Created by Hyowon Jeon on 6/27/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class LibraryCollectionView: UIView {
+    
+    //MARK: - Components
+    
+    lazy var libraryCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - UI
+    
+    private func setUI() {
+        libraryCollectionView.do {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            layout.minimumLineSpacing = 18
+            layout.minimumInteritemSpacing = 6
+            layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+            let cellWidth = (UIScreen.main.bounds.width - (6 * 2) - (20 * 2)) / 3
+            let cellHeight = cellWidth * 160 / 108 + 71
+            layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+            
+            $0.collectionViewLayout = layout
+            $0.showsVerticalScrollIndicator = false
+        }
+    }
+  
+    private func setHierarchy() {
+        self.addSubview(libraryCollectionView)
+    }
+    
+    private func setLayout() {
+        libraryCollectionView.snp.makeConstraints() {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
