60e1fe8c7dc61d3c3afaa3797c8e53a5d06b8c33
YunHak Lee <lyh990408@gmail.com>
2025-06-27 01:29:57 +0900

    [Design] #580 - 필터뷰 dismiss 버튼 기능 구현

--- WSSiOS/WSSiOS/Source/Presentation/Library/LibraryFilter/LibraryFilterViewController/LibraryFilterViewController.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Library/LibraryFilter/LibraryFilterViewController/LibraryFilterViewController.swift
@@ -15,11 +15,15 @@ import Then
 
 final class LibraryFilterViewController: UIViewController {
     
+    //MARK: - Properties
+    
+    private let disposeBag = DisposeBag()
+    
     //MARK: - Components
     
-    let rootView = LibraryFilterView()
+    private let rootView = LibraryFilterView()
     
-    // MARK: - Life Cycle
+    //MARK: - Life Cycle
     
     override func loadView() {
         view = rootView
@@ -28,10 +32,23 @@ final class LibraryFilterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        bindAction()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+    }
+    
+    //MARK: - Bind
+    
+    private func bindAction() {
+        rootView.dismissButton.rx.tap
+            .observe(on: MainScheduler.instance)
+            .bind(with: self, onNext: { owner, _ in
+                owner.dismissModalViewController()
+            })
+            .disposed(by: disposeBag)
         
     }
 }
