281a773c72fa879486753fe787558617bd73bcb1
YunHak Lee <lyh990408@gmail.com>
2025-06-27 03:08:50 +0900

    [Chore] #580 - 컴포넌트 네이밍 룰에 따라 수정

--- WSSiOS/WSSiOS/Source/Presentation/Library/LibraryFilter/LibraryFilterView/LibraryFilterAssistantView/LibraryFilterAttractivePointOptionButton.swift
+++ WSSiOS/WSSiOS/Source/Presentation/Library/LibraryFilter/LibraryFilterView/LibraryFilterAssistantView/LibraryFilterAttractivePointOptionButton.swift
@@ -19,7 +19,7 @@ final class LibraryFilterAttractivePointOptionButton: UIButton {
     //MARK: - UI Components
     
     private let stackView = UIStackView()
-    private let statusImage = UIImageView()
+    private let statusImageView = UIImageView()
     private let statusLabel = UILabel()
     
     //MARK: - Life Cycle
@@ -53,7 +53,7 @@ final class LibraryFilterAttractivePointOptionButton: UIButton {
             $0.isUserInteractionEnabled = false
         }
         
-        statusImage.do {
+        statusImageView.do {
             $0.image = attractivePoint.image.withRenderingMode(.alwaysTemplate)
             $0.tintColor = .wssGray100
             $0.contentMode = .scaleAspectFit
@@ -63,7 +63,7 @@ final class LibraryFilterAttractivePointOptionButton: UIButton {
     
     private func setHierarchy() {
         self.addSubviews(stackView)
-        stackView.addArrangedSubviews(statusImage,
+        stackView.addArrangedSubviews(statusImageView,
                                       statusLabel)
     }
     
@@ -76,7 +76,7 @@ final class LibraryFilterAttractivePointOptionButton: UIButton {
             $0.top.horizontalEdges.equalToSuperview()
         }
         
-        statusImage.snp.makeConstraints {
+        statusImageView.snp.makeConstraints {
             $0.size.equalTo(36)
         }
     }
