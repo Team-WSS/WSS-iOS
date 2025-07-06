a3f86650efe4e6fc8cf053264b79813338961807
YunHak Lee <lyh990408@gmail.com>
2025-07-07 00:55:14 +0900

    [Network] #587 - 내 피드 필터 적용 없이 기본 데이터 가져오도록 구현

--- WSSiOS/WSSiOS/Source/Data/Entity/Feed/TotalFeedListEntity.swift
+++ WSSiOS/WSSiOS/Source/Data/Entity/Feed/TotalFeedListEntity.swift
@@ -14,6 +14,70 @@ struct TotalFeedListEntity {
     let feeds: [TotalFeedEntity]
 }
 
+extension TotalFeedListEntity {
+    static func from(userFeedListEntity: UserFeedListEntity, myProfileEntity: MyProfileEntity, userId: Int) -> TotalFeedListEntity {
+        TotalFeedListEntity(
+            category: "all",
+            isLoadable: userFeedListEntity.isLoadable,
+            feeds: userFeedListEntity.feeds.map {
+                TotalFeedEntity(
+                    feedId: $0.feedId,
+                    userId: userId,
+                    nickname: myProfileEntity.nickname,
+                    avatarImage: myProfileEntity.avatarImageURL,
+                    createdDate: $0.createdDate,
+                    feedContent: $0.feedContent,
+                    likeCount: $0.likeCount,
+                    isLiked: $0.isLiked,
+                    commentCount: $0.commentCount,
+                    novelId: $0.novelId,
+                    title: $0.title,
+                    novelRatingCount: $0.novelRatingCount,
+                    novelRating: $0.novelRating,
+                    novelGenreColor: $0.novelGenreColor,
+                    novelGenreImage: $0.novelGenreImage,
+                    relevantCategories: $0.relevantCategories.joined(separator: ", "),
+                    isSpoiler: $0.isSpoiler,
+                    isModified: $0.isModified,
+                    isMyFeed: true,
+                    isPublic: $0.isPublic,
+                    thumbnailImageURL: URL(string: $0.thumbnailImage),
+                    hasImage: $0.hasImage,
+                    imageCount: $0.imageCount)
+            })
+    }
+}
+//struct TotalFeedEntity {
+//    let feedId: Int
+//    //let userId: Int
+//   // let nickname: String
+//   // let avatarImage: URL?
+//    let feedContent: String
+//    let createdDate: String
+//    let isSpoiler: Bool
+//    let isModified: Bool
+//    let isLiked: Bool
+//    let likeCount: Int
+//    let commentCount: Int
+//    let novelId: Int
+//    let title: String
+//
+//    let novelRatingCount: Int
+//    let novelRating: Float
+//    let novelGenreColor: UIColor
+//    let novelGenreImage: UIImage
+//
+//    let relevantCategories: String
+//    let isPublic: Bool
+//
+//    //let isMyFeed: Bool
+//
+//    let thumbnailImageURL: URL?
+//    let hasImage: Bool
+//    let imageCount: Int
+//}
+
+
 extension TotalFeedListResponse {
     func toEntity() -> TotalFeedListEntity {
         return TotalFeedListEntity(category: self.category,
