dd7b82ae50d00c6f28bec55bbc6c79b42acc8374
ena-isme <dev.shinena@gmail.com>
2025-08-01 12:02:00 +0900

    [Feat] #613 - 문의하기 링크 변경

--- WSSiOS/WSSiOS/Resource/Constants/URLs/URLs.swift
+++ WSSiOS/WSSiOS/Resource/Constants/URLs/URLs.swift
@@ -202,7 +202,7 @@ enum URLs {
     }
     
     enum Setting {
-        static let QNAInHompageURL = "https://websoso.notion.site/144600bd746881d4b012fbaf586c264d?pvs=105"
+        static let QNAInHompageURL = "https://helpwebsoso.notion.site/241a9688d1a381548c20dd314d0a0b0a"
         static let instaURL = "https://www.instagram.com/websoso_official/"
         static let termsURL = "https://websoso.notion.site/143600bd746880668556fb005fcef491?pvs=143"
         static let infoURL = "https://websoso.notion.site/143600bd74688050be18f4da31d9403e?pvs=4"
