35c1c3ddc9a21b564560253ab70a86b160e4328b
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-01-31 01:35:15 +0900

    [Fix] #650 - reload, pagination 로직 개선을 통한 피드 개수 오류 수정

--- WSSiOS/Source/Presentation/Feed/Feed/FeedViewModel/FeedPageContentViewModel.swift
+++ WSSiOS/Source/Presentation/Feed/Feed/FeedViewModel/FeedPageContentViewModel.swift
@@ -97,7 +97,7 @@ final class FeedPageContentViewModel: ViewModelType {
             .withLatestFrom(feedList)
             .flatMapLatest { feedList in
                 self.getFeedData(lastFeedId: self.lastFeedId,
-                                 size: feedList.isEmpty ? nil : feedList.count)
+                                 size: nil)
             }
             .subscribe(with: self, onNext: { owner, data in
                 owner.isLoadable = data.isLoadable
@@ -279,6 +279,14 @@ final class FeedPageContentViewModel: ViewModelType {
             })
             .disposed(by: disposeBag)
         
+        feedPageType
+            .skip(1)
+            .subscribe(with: self, onNext: { owner, _ in
+                owner.resetFeedPagingState()
+                owner.feedTableViewIsRefreshing.accept(())
+            })
+            .disposed(by: disposeBag)
+        
         return Output(
             feedPageType: feedPageType.asDriver(),
             myFeedCount: myFeedCount.asDriver(),
@@ -355,5 +363,14 @@ final class FeedPageContentViewModel: ViewModelType {
     func deleteFeed(_ feedId: Int) -> Observable<Void> {
         feedDetailRepository.deleteFeed(feedId: feedId)
             .observe(on: MainScheduler.instance)
+    }
+    
+    //MARK: - Method
+    
+    private func resetFeedPagingState() {
+        isLoadable = false
+        isFetching = false
+        lastFeedId = 0
+        feedList.accept([])
     }
 }
