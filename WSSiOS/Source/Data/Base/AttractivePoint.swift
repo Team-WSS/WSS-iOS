c8ad7eaba353b6d3bb9b19707050812c7e6217d6
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-03-11 15:29:44 +0900

    [Feat] #676 - 매력포인트 열거형 내 필력 추가 및 레이아웃 조정

--- WSSiOS/Source/Data/Base/AttractivePoint.swift
+++ WSSiOS/Source/Data/Base/AttractivePoint.swift
@@ -10,6 +10,7 @@ import UIKit
 enum AttractivePoint: String, CaseIterable, Codable {
     case worldview = "worldview"
     case material = "material"
+    case writingSkill = "writingskill"
     case character = "character"
     case relationship = "relationship"
     case vibe = "vibe"
@@ -18,6 +19,7 @@ enum AttractivePoint: String, CaseIterable, Codable {
         switch self {
         case .worldview: "세계관"
         case .material: "소재"
+        case .writingSkill: "필력"
         case .character: "캐릭터"
         case .relationship: "관계"
         case .vibe: "분위기"
@@ -28,6 +30,8 @@ enum AttractivePoint: String, CaseIterable, Codable {
         switch self {
         case .worldview: .icAttractiveWorldview
         case .material: .icAttractiveMaterial
+        //TODO: - 필력 아이콘 이미지 변경 필요
+        case .writingSkill: .icAttractiveVibe
         case .character: .icAttractiveCharacter
         case .relationship: .icAttractiveRelationship
         case .vibe: .icAttractiveVibe
