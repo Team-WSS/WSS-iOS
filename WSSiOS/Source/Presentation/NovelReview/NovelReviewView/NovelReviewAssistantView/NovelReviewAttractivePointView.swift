1cb8893eb5186c8c4d63992fe487cf5a68e17cbc
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-03-30 13:19:11 +0900

    [Fix] 1.5.0 - 작품 평가 매력포인트 collectionview 레이아웃 조정

--- WSSiOS/Source/Presentation/NovelReview/NovelReviewView/NovelReviewAssistantView/NovelReviewAttractivePointView.swift
+++ WSSiOS/Source/Presentation/NovelReview/NovelReviewView/NovelReviewAssistantView/NovelReviewAttractivePointView.swift
@@ -69,7 +69,7 @@ final class NovelReviewAttractivePointView: UIView {
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.top.equalTo(titleLabel.snp.bottom).offset(10)
             $0.bottom.equalToSuperview().inset(32)
-            $0.height.equalTo(35)
+            $0.height.equalTo(37)
         }
     }
 }
