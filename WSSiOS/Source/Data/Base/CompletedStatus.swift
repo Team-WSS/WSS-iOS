a8d62951e8cc432eb2bb719a52c28a438d47d040
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-04-29 19:06:08 +0900

    [Chore] #695 - 연재상태 Enum case 순서 변경

--- WSSiOS/Source/Data/Base/CompletedStatus.swift
+++ WSSiOS/Source/Data/Base/CompletedStatus.swift
@@ -8,24 +8,24 @@
 import Foundation
 
 enum CompletedStatus: String, CaseIterable {
+    case onGoing
     case completed
-    case notCompleted
     
     var description: String {
         switch self {
+        case .onGoing: return "연재중"
         case .completed: return "완결작"
-        case .notCompleted: return "연재중"
         }
     }
     
     var isCompleted: Bool {
         switch self {
+        case .onGoing: return false
         case .completed: return true
-        case .notCompleted: return false
         }
     }
     
     init(isCompleted: Bool) {
-        self = isCompleted ? .completed : .notCompleted
+        self = isCompleted ? .completed : .onGoing
     }
 }
