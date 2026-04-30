f73afbdefec3aa808ec7f12c89c1f745ba648584
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-04-30 18:07:15 +0900

    [Chore] #695 - UserFeedListEntity내 createdDate 포맷팅함수 제거

--- WSSiOS/Source/Data/Entity/UserFeedListEntity.swift
+++ WSSiOS/Source/Data/Entity/UserFeedListEntity.swift
@@ -62,7 +62,7 @@ extension UserFeedResponse {
         
         return UserFeedEntity(feedId: self.feedId,
                               feedContent: self.feedContent,
-                              createdDate: self.formattedDate(),
+                              createdDate: self.createdDate,
                               isSpoiler: self.isSpoiler,
                               isModified: self.isModified,
                               isLiked: self.isLiked,
@@ -77,21 +77,6 @@ extension UserFeedResponse {
                               thumbnailImage: thumbnailImageURL,
                               hasImage: hasImage,
                               imageCount: self.imageCount)
-    }
-    
-    private func formattedDate() -> String {
-        let inputDateFormatter = DateFormatter()
-        inputDateFormatter.dateFormat = "yyyy-MM-dd"
-        
-        guard let date = inputDateFormatter.date(from: self.createdDate) else {
-            return ""
-        }
-        
-        let outputDateFormatter = DateFormatter()
-        outputDateFormatter.locale = Locale(identifier: "ko_KR")
-        outputDateFormatter.dateFormat = "M월 d일"
-        
-        return outputDateFormatter.string(from: date)
     }
 }
 
