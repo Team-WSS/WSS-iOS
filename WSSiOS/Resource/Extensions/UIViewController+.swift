eae25f28b0e3933de9e0da7022a2b59f577853f0
Seoyeon Choi <lala1470@konkuk.ac.kr>
2026-04-30 15:47:05 +0900

    [Chore] #695 - navigationItem 버튼 liquid glass UI 제거
     및 navigationBar 불투명하게 되는 이슈사항 부분적 해결

--- WSSiOS/Resource/Extensions/UIViewController+.swift
+++ WSSiOS/Resource/Extensions/UIViewController+.swift
@@ -56,6 +56,10 @@ extension UIViewController {
         self.navigationItem.title = title
         self.navigationItem.leftBarButtonItem = left != nil ? UIBarButtonItem(customView: left!) : nil
         self.navigationItem.rightBarButtonItem = right != nil ? UIBarButtonItem(customView: right!) : nil
+        if #available(iOS 26.0, *) {
+            self.navigationItem.leftBarButtonItem?.hidesSharedBackground = true
+            self.navigationItem.rightBarButtonItem?.hidesSharedBackground = true
+        }
         setNavigationBarVisibleBeforeScroll(isVisible: isVisibleBeforeScroll)
     }
     
@@ -69,7 +73,7 @@ extension UIViewController {
             ]
             $0.shadowColor = .clear
         }
-        
+
         let whiteAppearance = UINavigationBarAppearance().then {
             $0.configureWithOpaqueBackground()
             $0.backgroundColor = .white
@@ -80,7 +84,7 @@ extension UIViewController {
             ]
             $0.shadowColor = .clear
         }
-        
+
         navigationItem.standardAppearance = whiteAppearance
         navigationItem.scrollEdgeAppearance = isVisible ? whiteAppearance : clearAppearance
     }
