010588bade536428b75efa8fb8a4983d2d900b9c
onesunny2 <ippne996@gmail.com>
2026-05-07 23:08:01 +0900

    [Feat] #703 - 최근 검색어 DTO 및 네트워크 레이어 구현

--- WSSiOS/Source/Data/DTO/SearchResult.swift
+++ WSSiOS/Source/Data/DTO/SearchResult.swift
@@ -45,10 +45,16 @@ struct SearchNovel: Codable {
     var interestCount: Int
     var novelRating: Float
     var novelRatingCount: Int
-    
+
     enum CodingKeys: String, CodingKey {
         case novelId, novelImage, interestCount, novelRating, novelRatingCount
         case novelTitle = "title"
         case novelAuthor = "author"
     }
+}
+
+/// 최근 검색어 조회 API
+struct RecentSearch: Codable {
+    let id: Int
+    let keyword: String
 }
