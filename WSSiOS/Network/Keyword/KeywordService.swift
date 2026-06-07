e68d509f4cc3826104d6f6530d9c34a6ad768f64
onesunny2 <ippne996@gmail.com>
2026-06-08 00:31:49 +0900

    [Feat] #708 - 인기 키워드 DTO 및 네트워크 레이어 구현

--- WSSiOS/Network/Keyword/KeywordService.swift
+++ WSSiOS/Network/Keyword/KeywordService.swift
@@ -11,16 +11,17 @@ import RxSwift
 
 protocol KeywordService {
     func searchKeyword(query: String?) -> Single<SearchKeywordResult>
+    func getPopularKeywords(size: Int) -> Single<[KeywordData]>
 }
 
 final class DefaultKeywordService: NSObject, Networking, KeywordService {
     func searchKeyword(query: String? = nil) -> RxSwift.Single<SearchKeywordResult> {
         var searchKeywordQueryItems: [URLQueryItem] = []
-        
+
         if let query {
             searchKeywordQueryItems.append(URLQueryItem(name: "query", value: query))
         }
-        
+
         do {
             let request = try makeHTTPRequest(method: .get,
                                               path: URLs.Keyword.searchKeyword,
@@ -33,6 +34,29 @@ final class DefaultKeywordService: NSObject, Networking, KeywordService {
             return tokenCheckURLSession.rx.data(request: request)
                 .map { try self.decode(data: $0,
                                        to: SearchKeywordResult.self) }
+                .asSingle()
+
+        } catch {
+            return Single.error(error)
+        }
+    }
+
+    func getPopularKeywords(size: Int) -> Single<[KeywordData]> {
+        let queryItems: [URLQueryItem] = [
+            URLQueryItem(name: "size", value: String(size))
+        ]
+
+        do {
+            let request = try makeHTTPRequest(method: .get,
+                                              path: URLs.Keyword.popularKeyword,
+                                              queryItems: queryItems,
+                                              headers: APIConstants.accessTokenHeader,
+                                              body: nil)
+
+            NetworkLogger.log(request: request)
+
+            return tokenCheckURLSession.rx.data(request: request)
+                .map { try self.decode(data: $0, to: PopularKeywords.self).keywords }
                 .asSingle()
 
         } catch {
--- WSSiOS/Source/Data/Repository/KeywordRepository.swift
+++ WSSiOS/Source/Data/Repository/KeywordRepository.swift
@@ -11,18 +11,25 @@ import RxSwift
 
 protocol KeywordRepository {
     func searchKeyword(query: String?) -> Observable<SearchKeywordResult>
+    func getPopularKeywords() -> Observable<[KeywordData]>
 }
 
 struct DefaultKeywordRepository: KeywordRepository {
-    
+
     private var keywordService: KeywordService
-    
+    private let popularKeywordSize = 7
+
     init(keywordService: KeywordService) {
         self.keywordService = keywordService
     }
-    
+
     func searchKeyword(query: String?) -> Observable<SearchKeywordResult> {
         return keywordService.searchKeyword(query: query)
+            .asObservable()
+    }
+
+    func getPopularKeywords() -> Observable<[KeywordData]> {
+        return keywordService.getPopularKeywords(size: popularKeywordSize)
             .asObservable()
     }
 }
